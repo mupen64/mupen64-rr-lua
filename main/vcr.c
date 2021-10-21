2eadfa97530d0877f49f34532f71ff936c0bd41d
mekb the turtle <mekbgithub@icloud.com>
2021-10-21 14:03:01 +1100

    Update vcr.c

--- main/vcr.c
+++ main/vcr.c
@@ -1212,6 +1212,8 @@ VCR_startRecord( const char *filename, unsigned short flags, const char *authorU
 
 	reserve_buffer_space(4096);
 
+    unsigned int previous_slot = savestates_get_slot();
+
     if(flags & MOVIE_START_FROM_SNAPSHOT)
     {
     	// save state
@@ -1260,6 +1262,8 @@ VCR_startRecord( const char *filename, unsigned short flags, const char *authorU
 
 	m_currentSample = 0;
 	m_currentVI = 0;
+
+    savestates_select_slot(previous_slot);
 
 	return 0;
 
