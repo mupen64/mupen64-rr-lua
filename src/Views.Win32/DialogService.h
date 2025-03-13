f91e632f77eb9b823429aa78a3732d40b5ee3ad5
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-03-13 07:50:23 +0100

    docs(DialogService): fix missing "return" in return comment for show_multiple_choice_dialog

--- src/Views.Win32/DialogService.h
+++ src/Views.Win32/DialogService.h
@@ -19,7 +19,7 @@ namespace DialogService
      * \param title The dialog title.
      * \param default_index The choice index to return when the user has chosen to not use modals.
      * \param hwnd The parent window. If nullptr, the main window will be used.
-     * \return The index of the chosen choice. If the user has chosen to not use modals, this function will <c>default_index</c> by default. If the user has chosen to not show the dialog again, this function will return the last choice.
+     * \return The index of the chosen choice. If the user has chosen to not use modals, this function will return <c>default_index</c> by default. If the user has chosen to not show the dialog again, this function will return the last choice.
      */
     size_t show_multiple_choice_dialog(const std::string& id, const std::vector<std::wstring>& choices, const wchar_t* str, const wchar_t* title = nullptr, size_t default_index = 0, core_dialog_type type = fsvc_warning, void* hwnd = nullptr);
 
