e4ccbc1e16fa84a6ca1edfb7f765806eac5888be
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-03-16 00:30:02 +0100

    chore: add missing copyright headers to LuaRegistry.cpp/h

--- src/Views.Win32/lua/LuaRegistry.cpp
+++ src/Views.Win32/lua/LuaRegistry.cpp
@@ -1,4 +1,10 @@
-﻿#include "stdafx.h"
+﻿/*
+ * Copyright (c) 2025, Mupen64 maintainers, contributors, and original authors (Hacktarux, ShadowPrince, linker).
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "stdafx.h"
 #include "LuaRegistry.h"
 #include <lua/modules/AVI.h>
 #include <lua/modules/D2D.h>
--- src/Views.Win32/lua/LuaRegistry.h
+++ src/Views.Win32/lua/LuaRegistry.h
@@ -1,4 +1,10 @@
-﻿#pragma once
+﻿/*
+ * Copyright (c) 2025, Mupen64 maintainers, contributors, and original authors (Hacktarux, ShadowPrince, linker).
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#pragma once
 
 /**
  * \brief A module responsible for implementing functionality related to the Lua function registry.
@@ -9,4 +15,4 @@ namespace LuaRegistry
      * \brief Registers the standard Lua functions along with Mupen's functions to the specified state.
      */
     void register_functions(lua_State* L);
-}
+} // namespace LuaRegistry
