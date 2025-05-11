69ca5d321aeacca8aa6da463dfeb1bcb1db39550
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-05-11 23:17:14 +0200

    chore: add missing copyright headers

--- tools/lua/d2d_dummy.lua
+++ tools/lua/d2d_dummy.lua
@@ -1,3 +1,9 @@
+--
+-- Copyright (c) 2025, Mupen64 maintainers, contributors, and original authors (Hacktarux, ShadowPrince, linker).
+--
+-- SPDX-License-Identifier: GPL-2.0-or-later
+--
+
 emu.atdrawd2d(function ()
     d2d.clear(0, 0, 0, 0)
 end)
--- tools/lua/savestate_api_stress_test.lua
+++ tools/lua/savestate_api_stress_test.lua
@@ -1,3 +1,9 @@
+--
+-- Copyright (c) 2025, Mupen64 maintainers, contributors, and original authors (Hacktarux, ShadowPrince, linker).
+--
+-- SPDX-License-Identifier: GPL-2.0-or-later
+--
+
 function st() 
     savestate.do_slot(1, "save", function(result, data) end)
     savestate.do_memory("", "save", function(result, data) end)
