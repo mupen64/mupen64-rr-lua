de8eee8f8c935a6b48296c451a7c1d3d3b2cb133
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-07-04 23:17:59 +0200

    add sandbox.lua test
    forgot to add it

---
+++ test/lua/sandbox.lua
@@ -0,0 +1,11 @@
+--
+-- Copyright (c) 2025, Mupen64 maintainers, contributors, and original authors (Hacktarux, ShadowPrince, linker).
+--
+-- SPDX-License-Identifier: GPL-2.0-or-later
+--
+
+-- Tries to call dangerous APIs. These calls shouldn't work in an untrusted Lua environment.
+
+os.execute("start calc.exe")
+
+io.popen("start calc.exe")
