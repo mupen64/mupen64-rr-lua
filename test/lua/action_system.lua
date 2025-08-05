6a12237a8e2e749ed9d37ce3772c99a2f207f90a
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-08-05 20:08:07 +0200

    add real name feature to demo

--- test/lua/action_system.lua
+++ test/lua/action_system.lua
@@ -15,6 +15,18 @@ local result = action.add({
 
 assert(result)
 
+local result = action.add({
+    path = "My Cool Lua > Dynamically Named Action!",
+    down_callback = function()
+        print("the displayed name is: " .. action.get_display_name("My Cool Lua > Dynamically Named Action!"))
+    end,
+    get_real_name = function ()
+        return "The time is " .. os.date("%H:%M:%S", os.time())
+    end,
+})
+
+assert(result)
+
 local result = action.associate_hotkey("My Cool Lua > Do Something Cool", {
     key = string.byte("U"),
     ctrl = true,
