a20bea11108995edc24df6853900e2290eb0739d
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-08-07 01:00:03 +0200

    improve action API demo

--- test/lua/action_system.lua
+++ test/lua/action_system.lua
@@ -4,46 +4,58 @@
 -- SPDX-License-Identifier: GPL-2.0-or-later
 --
 
--- Sets up some actions.
+-- Demonstrates the usage of the action API.
+
+local display_name = ""
+
+local SAY_HELLO_WORLD_ACTION = "Action API Demo > Print 'Hello World!'"
+local CHANGE_NAME_ACTION = "Action API Demo > Change Name of This Action..."
+local CHANGE_HOTKEY_ACTION = "Action API Demo > Change Hotkey..."
+local RANDOM_ITEM_ACTION = "Action API Demo > Click Child to Remove It > Item %d"
 
 assert(action.add({
-    path = "My Cool Lua > Do Something Cool",
+    path = SAY_HELLO_WORLD_ACTION,
     down_callback = function()
         print("Hello World!")
     end
 }))
 
 assert(action.add({
-    path = "My Cool Lua > Dynamically Named Action! ---",
+    path = CHANGE_NAME_ACTION,
     down_callback = function()
-        print("the displayed name is: " .. action.get_display_name("My Cool Lua > Dynamically Named Action! ---"))
+        local str = input.prompt("Action display name",
+            action.get_display_name(CHANGE_NAME_ACTION))
+        if str then
+            display_name = str
+        end
     end,
     get_display_name = function()
-        return "The time is " .. os.date("%H:%M:%S", os.time())
+        return display_name
     end,
 }))
 
-assert(action.associate_hotkey("My Cool Lua > Do Something Cool", {
-    key = string.byte("U"),
-    ctrl = true,
-}, false))
-
 assert(action.add({
-    path = "My Cool Lua > Change hotkey of first action",
+    path = CHANGE_HOTKEY_ACTION,
     down_callback = function()
-        local hotkey = hotkey.prompt("Change the hotkey of blah blah something something")
+        local hotkey = hotkey.prompt("Change the hotkey of that other action")
         if hotkey then
-            assert(action.associate_hotkey("My Cool Lua > Do Something Cool", hotkey, true))
+            assert(action.associate_hotkey(CHANGE_NAME_ACTION, hotkey, true))
         end
     end
 }))
 
 for i = 1, 10, 1 do
+    local path = string.format(RANDOM_ITEM_ACTION, i)
     assert(action.add({
-        path = "My Cool Lua > Some Items > Item " .. i,
+        path = path,
         down_callback = function()
-            local removed_items = action.remove("My Cool Lua > Some Items > Item " .. i)
+            local removed_items = action.remove(path)
             print("removed items", removed_items)
         end
     }))
 end
+
+assert(action.associate_hotkey(CHANGE_NAME_ACTION, {
+    key = string.byte("U"),
+    ctrl = true,
+}, false))
