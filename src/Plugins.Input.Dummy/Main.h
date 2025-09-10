3c3fcb2319ff2519fda76027ef6e46bdd30e3e41
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-09-10 18:52:50 +0200

    add "No Plugin" projects

---
+++ src/Plugins.Input.Dummy/Main.h
@@ -0,0 +1,31 @@
+﻿/*
+ * Copyright (c) 2025, Mupen64 maintainers, contributors, and original authors (Hacktarux, ShadowPrince, linker).
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#pragma once
+
+#define PLUGIN_VERSION L"1.0.0"
+
+#ifdef _M_X64
+#define PLUGIN_ARCH L" x64"
+#else
+#define PLUGIN_ARCH L" x86"
+#endif
+
+#ifdef _DEBUG
+#define PLUGIN_TARGET L" Debug"
+#else
+#define PLUGIN_TARGET L" Release"
+#endif
+
+#define PLUGIN_NAME L"No Input " PLUGIN_VERSION PLUGIN_ARCH PLUGIN_TARGET
+
+#define EXPORT __declspec(dllexport)
+#define CALL _cdecl
+
+extern "C" {
+EXPORT void CALL GetDllInfo(core_plugin_info* info);
+EXPORT void CALL DllAbout(void* hParent);
+}
