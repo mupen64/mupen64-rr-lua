d88efec7e80ef12f09ea03044325d116e3bebe4a
jgcodes2020 <j349guo@uwaterloo.ca>
2025-10-11 18:24:52 -0400

    fix plugin output directory, make builds work in vscode

--- toolchains/vcpkg-win64.cmake
+++ toolchains/vcpkg-win64.cmake
@@ -8,30 +8,43 @@ SPDX-License-Identifier: GPL-2.0-or-later
 # =========================================================
 
 if(NOT ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
-    message(FATAL_ERROR "This toolchain file is designed for use on Windows.")
+  message(FATAL_ERROR "This toolchain file is designed for use on Windows.")
 endif()
 
-if (NOT DEFINED ENV{VSCMD_VER})
-    message(FATAL_ERROR "Please build from a Visual Studio developer environment (cmd.exe or PowerShell)")
+if(NOT DEFINED ENV{VCINSTALLDIR})
+  message(FATAL_ERROR "Please build from a Visual Studio developer environment (cmd.exe or PowerShell)")
 endif()
 
-set(
-    MUPEN64_VCPKG_TOOLCHAIN "$ENV{VSINSTALLDIR}VC\\vcpkg\\scripts\\buildsystems\\vcpkg.cmake"
+# determine host and target architectures based on folder structure of MSVC.
+# the path ends with Host<host_arch>/<target_arch>/cl.exe for a given host/target arch.
+find_program(_cl_exe_path cl.exe NO_CACHE)
+message(STATUS "Found cl.exe at ${_cl_exe_path}")
+
+if("${_cl_exe_path}" MATCHES [=[/Host(.+)/(.+)/cl\.exe$]=])
+  set(_vs_host_arch "${CMAKE_MATCH_1}")
+  set(_vs_target_arch "${CMAKE_MATCH_2}")
+else()
+  message(FATAL_ERROR "Failed to determine host/target architectures!")
+endif()
+
+# Find the toolchain file using the current environment
+if(NOT DEFINED CACHE{MUPEN64RR_VCPKG_TOOLCHAIN})
+  set(
+    MUPEN64RR_VCPKG_TOOLCHAIN "$ENV{VCINSTALLDIR}\\vcpkg\\scripts\\buildsystems\\vcpkg.cmake"
     CACHE INTERNAL "Location of vcpkg's toolchain file."
-)
-if (NOT EXISTS "${MUPEN64_VCPKG_TOOLCHAIN}")
-    message(FATAL_ERROR "Expected vcpkg.cmake at ${MUPEN64_VCPKG_TOOLCHAIN}")
+  )
+endif()
+
+if(NOT EXISTS "${MUPEN64RR_VCPKG_TOOLCHAIN}")
+  message(FATAL_ERROR "Expected vcpkg.cmake at ${MUPEN64RR_VCPKG_TOOLCHAIN}")
 endif()
 
 # set some necessary settings to get compilation to work properly
 set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>" CACHE INTERNAL "MSVCRT variant needed to get things to work.")
-add_compile_definitions(UNICODE)
 
 # setup a few last values for vcpkg
-set(VCPKG_TARGET_TRIPLET "$ENV{VSCMD_ARG_TGT_ARCH}-windows-static" CACHE INTERNAL "target triplet for vcpkg")
-
-
-message(STATUS "VS architecture set to: $ENV{VSCMD_ARG_TGT_ARCH}")
+set(VCPKG_TARGET_TRIPLET "${_vs_target_arch}-windows-static" CACHE INTERNAL "target triplet for vcpkg")
+message(STATUS "VS architecture set to: ${_vs_target_arch}")
 
 # hand off the rest to vcpkg
-include(${MUPEN64_VCPKG_TOOLCHAIN})
+include(${MUPEN64RR_VCPKG_TOOLCHAIN})
