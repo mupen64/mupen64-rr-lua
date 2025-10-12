226a3e7f812926c8c38900ce923c380ba65f9e3e
jgcodes2020 <j349guo@uwaterloo.ca>
2025-10-11 22:43:35 -0400

    get asan in to debug the crash

--- toolchains/vcpkg-win64.cmake
+++ toolchains/vcpkg-win64.cmake
@@ -15,6 +15,23 @@ if(NOT DEFINED ENV{VCINSTALLDIR})
   message(FATAL_ERROR "Please build from a Visual Studio developer environment (cmd.exe or PowerShell)")
 endif()
 
+# OPTIONS
+# =========================================================
+set(_toolchain_sanitizer_keys OFF ASAN)
+set(_toolchain_sanitizer_values "" "-asan")
+
+set(MUPEN64RR_USE_SANITIZER OFF CACHE STRING "Specifies a sanitizer to compile with. [${_toolchain_sanitizer_keys}]")
+
+# validate sanitizer option
+list(FIND _toolchain_sanitizer_keys "${MUPEN64RR_USE_SANITIZER}" _key_index)
+if ("${_key_index}" LESS 0)
+  message(FATAL_ERROR "expected MUPEN64RR_USE_SANITIZER to be one of [${_toolchain_sanitizer_keys}]")
+endif()
+list(GET _toolchain_sanitizer_values "${_key_index}" _vcpkg_san_suffix)
+
+# CONFIGURATION
+# =========================================================
+
 # determine host and target architectures based on folder structure of MSVC.
 # the path ends with Host<host_arch>/<target_arch>/cl.exe for a given host/target arch.
 find_program(_cl_exe_path cl.exe NO_CACHE)
@@ -42,9 +59,20 @@ endif()
 # set some necessary settings to get compilation to work properly
 set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>" CACHE INTERNAL "MSVCRT variant needed to get things to work.")
 
+set(CMAKE_C_FLAGS_DEBUG_INIT)
+set(CMAKE_CXX_FLAGS_DEBUG_INIT)
+
+if ("${MUPEN64RR_USE_SANITIZER}" STREQUAL "ASAN")
+  list(APPEND CMAKE_C_FLAGS_DEBUG_INIT "/fsanitize=address")
+  list(APPEND CMAKE_CXX_FLAGS_DEBUG_INIT "/fsanitize=address")
+endif()
+
 # setup a few last values for vcpkg
-set(VCPKG_TARGET_TRIPLET "${_vs_target_arch}-windows-static" CACHE INTERNAL "target triplet for vcpkg")
+set(VCPKG_TARGET_TRIPLET "${_vs_target_arch}-windows-static${_vcpkg_san_suffix}" CACHE INTERNAL "target triplet for vcpkg")
+set(VCPKG_OVERLAY_TRIPLETS "${CMAKE_CURRENT_LIST_DIR}/vcpkg-triplets")
 message(STATUS "VS architecture set to: ${_vs_target_arch}")
+
+
 
 # hand off the rest to vcpkg
 include(${MUPEN64RR_VCPKG_TOOLCHAIN})
