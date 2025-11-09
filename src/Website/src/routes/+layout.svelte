85084d349e8706ec786f5aa88da8c479316a9017
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-11-09 20:19:33 +0100

    fix mobile layout

--- src/Website/src/routes/+layout.svelte
+++ src/Website/src/routes/+layout.svelte
@@ -12,20 +12,20 @@
 	<link rel="icon" href={mupen64} />
 </svelte:head>
 
-<div class="bg-surface-2 h-15 flex flex-row items-center justify-center gap-4">
-	<a href="/" class="flex flex-row items-center gap-2 py-2 hover:underline">
-		<img src={mupen64} alt="Mupen64 Logo" class="object-fit w-7" />
-		<span>Mupen64</span>
-	</a>
-
+<div
+	class="bg-surface-2 h-15 flex flex-row items-center justify-center gap-4 overflow-x-auto overflow-y-hidden whitespace-nowrap"
+>
 	<div class="group">
-		<a href="/docs/win" class=" flex flex-row items-center gap-2 py-2 hover:underline">
-			<span>General Docs</span>
+		<a href="/" class="flex flex-row items-center gap-2 py-2 hover:underline">
+			<img src={mupen64} alt="Mupen64 Logo" class="object-fit w-7" />
+			<span class="hidden md:block">Mupen64</span>
 		</a>
 
-		<div
-			class="bg-surface-light-3 dark:bg-surface-3 absolute hidden flex-col p-2 group-hover:flex"
-		>
+		<div class="bg-surface-light-3 dark:bg-surface-3 absolute hidden flex-col p-2 group-hover:flex">
+			<a href="/docs/lua" class="flex flex-row items-center gap-2 py-2 hover:underline">
+				<span>Lua Docs</span>
+			</a>
+			<hr class="border border-dashed border-red-600" />
 			{#each data.files as file}
 				<a href="/docs/win/{file}" class="flex flex-row items-center gap-2 py-2 hover:underline">
 					<span>{doc_filesystem_to_friendly_name(file)}</span>
@@ -34,21 +34,17 @@
 		</div>
 	</div>
 
-	<a href="/docs/lua" class="flex flex-row items-center gap-2 py-2 hover:underline">
-		<span>Lua Docs</span>
-	</a>
-
-	<p class="opacity-70">&middot;</p>
 	<a href="/sm64luaredux" class="flex flex-row items-center gap-2 py-2 hover:underline">
 		<img src={sm64luaredux} alt="SM64 Lua Redux Logo" class="object-fit w-7" />
-		<span>SM64 Lua Redux</span>
+		<span class="hidden md:block">SM64 Lua Redux</span>
 	</a>
 
 	<a href="/ugui" class="flex flex-row items-center gap-2 py-2 hover:underline">
 		<img src={ugui} alt="ugui Logo" class="object-fit w-7" />
-		<span>ugui</span>
+		<span class="hidden md:block">ugui</span>
 	</a>
 </div>
+
 {@render children?.()}
 
 <footer class="w-full">
