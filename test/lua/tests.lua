0ef18d46fbd0f2bc9816a6afe58c545c07c50540
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-11-18 21:26:24 +0100

    add savestate.loadfile test

--- test/lua/tests.lua
+++ test/lua/tests.lua
@@ -109,6 +109,19 @@ lust.describe('mupen64', function()
 
                 savestate.do_file = __prev_savestate_do_file
             end)
+            lust.it('loadfile_calls_dofile', function()
+                local FILENAME = "test.st"
+
+                __prev_savestate_do_file = savestate.do_file
+                savestate.do_file = function(filename, mode)
+                    lust.expect(filename).to.equal(FILENAME)
+                    lust.expect(mode).to.equal("load")
+                end
+
+                savestate.loadfile(FILENAME)
+
+                savestate.do_file = __prev_savestate_do_file
+            end)
         end)
         lust.describe('input', function()
             lust.it('map_virtual_key_ex_exists_and_prints_deprecation', function()
