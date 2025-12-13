67860515823609a8ceca20bc1f5fb09b50fbcc73
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-12-13 13:37:36 +0100

    chore: bump version to 1.3.0-9

--- src/Views.Win32/Main.h
+++ src/Views.Win32/Main.h
@@ -8,7 +8,7 @@
 
 #include <components/Dispatcher.h>
 
-#define CURRENT_VERSION L"1.3.0-8"
+#define CURRENT_VERSION L"1.3.0-9"
 
 #define WM_FOCUS_MAIN_WINDOW (WM_USER + 17)
 #define WM_EXECUTE_DISPATCHER (WM_USER + 18)
