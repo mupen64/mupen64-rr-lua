0fbaf4c2a9544740e294341a40a0f11b1b388e09
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-12-21 13:28:29 +0100

    change repo link

--- src/Plugins.Win32.TASInput/TASInput.cpp
+++ src/Plugins.Win32.TASInput/TASInput.cpp
@@ -187,7 +187,7 @@ EXPORT void CALL DllAbout(void* hParent)
     const auto msg = PLUGIN_NAME L"\n"
                                  L"Part of the Mupen64 project family."
                                  L"\n\n"
-                                 L"https://github.com/mupen64/TASInput";
+                                 L"https://github.com/mupen64/mupen64-rr-lua";
 
     MessageBox((HWND)hParent, msg, L"About", MB_ICONINFORMATION | MB_OK);
 }
