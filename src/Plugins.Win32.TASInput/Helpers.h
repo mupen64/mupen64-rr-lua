298d4dbd8e47477bb4599ebe72a3ad6b30bd2309
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-12-21 13:26:03 +0100

    move tasinput assert macros to helpers.h

--- src/Plugins.Win32.TASInput/Helpers.h
+++ src/Plugins.Win32.TASInput/Helpers.h
@@ -35,3 +35,29 @@ static bool is_mouse_over_control(const HWND hwnd, const int id)
 {
     return is_mouse_over_control(GetDlgItem(hwnd, id));
 }
+
+static void runtime_assert_fail(const std::wstring &message)
+{
+#if defined(_DEBUG)
+    __debugbreak();
+#endif
+    MessageBox(nullptr, message.c_str(), L"Failed Runtime Assertion", MB_ICONERROR | MB_OK);
+    std::terminate();
+}
+
+/**
+ * \brief Asserts a condition at runtime.
+ */
+#define RT_ASSERT(condition, message)                                                                                  \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        if (!(condition))                                                                                              \
+        {                                                                                                              \
+            runtime_assert_fail(message);                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+/**
+ * \brief Asserts that an HRESULT is SUCCESS at runtime.
+ */
+#define RT_ASSERT_HR(hr, message) RT_ASSERT(!FAILED(hr), message)
--- src/Plugins.Win32.TASInput/Main.h
+++ src/Plugins.Win32.TASInput/Main.h
@@ -12,30 +12,3 @@ extern core_plugin_extended_funcs* g_ef;
 #define PLUGIN_NAME VERSION_NAME_HELPER_GEN_NAME(L"TAS Input", L"2.0.0")
 
 #define NUMBER_OF_CONTROLS 4
-
-static void runtime_assert_fail(const std::wstring& message)
-{
-#if defined(_DEBUG)
-    __debugbreak();
-#endif
-    MessageBox(nullptr, message.c_str(), L"Failed Runtime Assertion", MB_ICONERROR | MB_OK);
-    std::terminate();
-}
-
-/**
- * \brief Asserts a condition at runtime.
- */
-#define RT_ASSERT(condition, message)     \
-    do                                    \
-    {                                     \
-        if (!(condition))                 \
-        {                                 \
-            runtime_assert_fail(message); \
-        }                                 \
-    }                                     \
-    while (0)
-
-/**
- * \brief Asserts that an HRESULT is SUCCESS at runtime.
- */
-#define RT_ASSERT_HR(hr, message) RT_ASSERT(!FAILED(hr), message)
