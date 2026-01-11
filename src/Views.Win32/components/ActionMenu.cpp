51b09ca29e50453d374f8f9a514f292c8b630157
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2026-01-11 12:11:06 +0100

    hand invocation off to ParameterPalette when trying to invoke parameterized actions via ActionMenu

--- src/Views.Win32/components/ActionMenu.cpp
+++ src/Views.Win32/components/ActionMenu.cpp
@@ -8,6 +8,7 @@
 #include <ActionManager.h>
 #include <Messenger.h>
 #include <components/ActionMenu.h>
+#include <components/ParameterPalette.h>
 
 const auto MANAGED_MENU_CTX = L"Mupen64_ManagedMenuContext";
 
@@ -198,6 +199,15 @@ static bool handle_menu_interaction(t_action_menu_context &ctx, const size_t id)
         return false;
     }
 
+    const auto params = ActionManager::get_params(found_action_path);
+
+    // Has params: hand off to ParameterPalette.
+    if (!params.empty())
+    {
+        ParameterPalette::show(found_action_path);
+        return true;
+    }
+    
     ActionManager::invoke(found_action_path);
 
     return true;
