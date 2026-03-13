e0d9c65c9a63503c0c64007089bbc4cc25c87486
Frame <nafoizret@web.de>
2026-03-13 14:31:06 +0100

    fix whitespace issues in Lua doc build script

--- docs/lua/build_documentation.bat
+++ docs/lua/build_documentation.bat
@@ -6,7 +6,7 @@ for /d %%D in ("%USERPROFILE%\.vscode\extensions\sumneko.lua*") do (
 )
 
 :: generate the documentation using the current directory to find api.lua
-%serverpath%\bin\lua-language-server.exe --doc "%~dp0..\..\src\Lua\api.lua"
+"%serverpath%\bin\lua-language-server.exe" --doc "%~dp0..\..\src\Lua\api.lua"
 
 :: copy the documentation to the export directory
 move "%~dp0..\..\doc.json" "%~dp0export\doc.json"
