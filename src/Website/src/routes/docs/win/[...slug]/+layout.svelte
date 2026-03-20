b4a96d208f902d960d302f319898652484f11eda
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2026-03-20 22:05:19 +0100

    fix(Website): remove now-redundant notice about early versions being shown

--- src/Website/src/routes/docs/win/[...slug]/+layout.svelte
+++ src/Website/src/routes/docs/win/[...slug]/+layout.svelte
@@ -3,9 +3,3 @@
 </script>
 
 {@render children?.()}
-
-<p class="mx-auto max-w-6xl text-center pb-16">
-	⚠️ This wiki provides information about Mupen64 1.3.0-19.
-	<br />
-	It may be updated before new versions are released.
-</p>
