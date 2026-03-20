06beaef0e97adc463e602b2141803ef611a53903
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2026-03-20 22:03:37 +0100

    bump mupen version to 1.3.0-20

--- src/Website/src/routes/docs/win/[...slug]/+layout.svelte
+++ src/Website/src/routes/docs/win/[...slug]/+layout.svelte
@@ -5,7 +5,7 @@
 {@render children?.()}
 
 <p class="mx-auto max-w-6xl text-center pb-16">
-	⚠️ This wiki provides information about Mupen64 1.3.0-19.
+	⚠️ This wiki provides information about Mupen64 1.3.0-20.
 	<br />
 	It may be updated before new versions are released.
 </p>
