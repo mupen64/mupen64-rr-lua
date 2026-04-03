073a768ce44403d3296c29059200cc2f8b166d16
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2026-04-03 23:15:42 +0200

    simplify naming

--- src/Views.Win32/lua/presenters/DCompPresenter.cpp
+++ src/Views.Win32/lua/presenters/DCompPresenter.cpp
@@ -15,19 +15,19 @@ bool DCompPresenter::init(HWND hwnd)
     GetClientRect(hwnd, &rect);
     m_size = {(UINT32)rect.right - rect.left, (UINT32)rect.bottom - rect.top};
 
-    CreateDXGIFactory2(0, IID_PPV_ARGS(dxgi_factory.GetAddressOf()));
-    dxgi_factory->EnumAdapters1(0, dxgi_adapter.GetAddressOf());
+    CreateDXGIFactory2(0, IID_PPV_ARGS(m_dxgi_factory.GetAddressOf()));
+    m_dxgi_factory->EnumAdapters1(0, m_dxgi_adapter.GetAddressOf());
 
-    D3D11CreateDevice(dxgi_adapter.Get(), D3D_DRIVER_TYPE_UNKNOWN, nullptr,
+    D3D11CreateDevice(m_dxgi_adapter.Get(), D3D_DRIVER_TYPE_UNKNOWN, nullptr,
                       D3D11_CREATE_DEVICE_BGRA_SUPPORT | D3D11_CREATE_DEVICE_SINGLETHREADED, nullptr, 0,
-                      D3D11_SDK_VERSION, &d3d11_device, nullptr, d3d_dc.GetAddressOf());
+                      D3D11_SDK_VERSION, &m_d3d_device, nullptr, m_d3d_dc.GetAddressOf());
 
-    d3d11_device->QueryInterface(dxgi_device.GetAddressOf());
-    dxgi_device->SetMaximumFrameLatency(1);
+    m_d3d_device->QueryInterface(m_dxgi_device.GetAddressOf());
+    m_dxgi_device->SetMaximumFrameLatency(1);
 
-    DCompositionCreateDevice(dxgi_device.Get(), IID_PPV_ARGS(comp_device.GetAddressOf()));
-    comp_device->CreateTargetForHwnd(hwnd, true, comp_target.GetAddressOf());
-    comp_device->CreateVisual(comp_visual.GetAddressOf());
+    DCompositionCreateDevice(m_dxgi_device.Get(), IID_PPV_ARGS(m_comp_device.GetAddressOf()));
+    m_comp_device->CreateTargetForHwnd(hwnd, true, m_comp_target.GetAddressOf());
+    m_comp_device->CreateVisual(m_comp_visual.GetAddressOf());
 
     DXGI_SWAP_CHAIN_DESC1 swapdesc{};
     swapdesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
@@ -39,14 +39,15 @@ bool DCompPresenter::init(HWND hwnd)
     swapdesc.Width = m_size.width;
     swapdesc.Height = m_size.height;
 
-    dxgi_factory->CreateSwapChainForComposition(d3d11_device.Get(), &swapdesc, nullptr, dxgi_swapchain.GetAddressOf());
-    comp_visual->SetContent(dxgi_swapchain.Get());
-    comp_target->SetRoot(comp_visual.Get());
+    m_dxgi_factory->CreateSwapChainForComposition(m_d3d_device.Get(), &swapdesc, nullptr,
+                                                  m_dxgi_swapchain.GetAddressOf());
+    m_comp_visual->SetContent(m_dxgi_swapchain.Get());
+    m_comp_target->SetRoot(m_comp_visual.Get());
 
-    D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, {}, d2d_factory.GetAddressOf());
-    d2d_factory->CreateDevice(dxgi_device.Get(), d2d_device.GetAddressOf());
-    d2d_device->CreateDeviceContext(D2D1_DEVICE_CONTEXT_OPTIONS_ENABLE_MULTITHREADED_OPTIMIZATIONS,
-                                    d2d_dc.GetAddressOf());
+    D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, {}, m_d2d_factory.GetAddressOf());
+    m_d2d_factory->CreateDevice(m_dxgi_device.Get(), m_d2d_device.GetAddressOf());
+    m_d2d_device->CreateDeviceContext(D2D1_DEVICE_CONTEXT_OPTIONS_ENABLE_MULTITHREADED_OPTIMIZATIONS,
+                                      m_d2d_dc.GetAddressOf());
 
     D3D11_TEXTURE2D_DESC desc{};
     desc.Width = m_size.width;
@@ -59,26 +60,26 @@ bool DCompPresenter::init(HWND hwnd)
     desc.BindFlags = D3D11_BIND_RENDER_TARGET;
     desc.MiscFlags = D3D11_RESOURCE_MISC_GDI_COMPATIBLE;
 
-    d3d11_device->CreateTexture2D(&desc, nullptr, d3d11_gdi_tex.GetAddressOf());
-    d3d11_gdi_tex->QueryInterface(dxgi_surface.GetAddressOf());
+    m_d3d_device->CreateTexture2D(&desc, nullptr, m_d3d_gdi_tex.GetAddressOf());
+    m_d3d_gdi_tex->QueryInterface(m_dxgi_surface.GetAddressOf());
 
     const UINT dpi = GetDpiForWindow(hwnd);
     const D2D1_BITMAP_PROPERTIES1 props =
         D2D1::BitmapProperties1(D2D1_BITMAP_OPTIONS_TARGET | D2D1_BITMAP_OPTIONS_CANNOT_DRAW,
                                 D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED), dpi, dpi);
 
-    d2d_dc->CreateBitmapFromDxgiSurface(dxgi_surface.Get(), props, d2d1_bitmap.GetAddressOf());
-    d2d_dc->SetTarget(d2d1_bitmap.Get());
+    m_d2d_dc->CreateBitmapFromDxgiSurface(m_dxgi_surface.Get(), props, m_d2d_bitmap.GetAddressOf());
+    m_d2d_dc->SetTarget(m_d2d_bitmap.Get());
 
-    dxgi_swapchain->GetBuffer(1, IID_PPV_ARGS(d3d11_front_buffer.GetAddressOf()));
-    dxgi_surface->QueryInterface(d3d11_surface.GetAddressOf());
+    m_dxgi_swapchain->GetBuffer(1, IID_PPV_ARGS(m_d3d_front_buffer.GetAddressOf()));
+    m_dxgi_surface->QueryInterface(m_d3d_surface.GetAddressOf());
 
     return true;
 }
 
 ID2D1RenderTarget *DCompPresenter::dc() const
 {
-    return d2d_dc.Get();
+    return m_d2d_dc.Get();
 }
 
 D2D1_SIZE_U DCompPresenter::size()
@@ -93,15 +94,15 @@ void DCompPresenter::resize(D2D1_SIZE_U size)
     m_size = size;
 
     // 1. Release size-dependent resources that must be recreated after a swapchain resize
-    d2d_dc->SetTarget(nullptr);
-    d2d1_bitmap.Reset();
-    dxgi_surface.Reset();
-    d3d11_front_buffer.Reset();
-    d3d11_surface.Reset();
-    d3d11_gdi_tex.Reset();
+    m_d2d_dc->SetTarget(nullptr);
+    m_d2d_bitmap.Reset();
+    m_dxgi_surface.Reset();
+    m_d3d_front_buffer.Reset();
+    m_d3d_surface.Reset();
+    m_d3d_gdi_tex.Reset();
 
     // 2. Resize the swapchain buffers
-    dxgi_swapchain->ResizeBuffers(2, size.width, size.height, DXGI_FORMAT_B8G8R8A8_UNORM, 0);
+    m_dxgi_swapchain->ResizeBuffers(2, size.width, size.height, DXGI_FORMAT_B8G8R8A8_UNORM, 0);
 
     // 3. Recreate the GDI-compatible texture at the new size
     D3D11_TEXTURE2D_DESC desc{};
@@ -115,8 +116,8 @@ void DCompPresenter::resize(D2D1_SIZE_U size)
     desc.BindFlags = D3D11_BIND_RENDER_TARGET;
     desc.MiscFlags = D3D11_RESOURCE_MISC_GDI_COMPATIBLE;
 
-    d3d11_device->CreateTexture2D(&desc, nullptr, d3d11_gdi_tex.GetAddressOf());
-    d3d11_gdi_tex->QueryInterface(dxgi_surface.GetAddressOf());
+    m_d3d_device->CreateTexture2D(&desc, nullptr, m_d3d_gdi_tex.GetAddressOf());
+    m_d3d_gdi_tex->QueryInterface(m_dxgi_surface.GetAddressOf());
 
     // 4. Recreate the D2D bitmap target from the new DXGI surface
     const UINT dpi = GetDpiForWindow(m_hwnd);
@@ -124,35 +125,35 @@ void DCompPresenter::resize(D2D1_SIZE_U size)
         D2D1::BitmapProperties1(D2D1_BITMAP_OPTIONS_TARGET | D2D1_BITMAP_OPTIONS_CANNOT_DRAW,
                                 D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED), dpi, dpi);
 
-    d2d_dc->CreateBitmapFromDxgiSurface(dxgi_surface.Get(), props, d2d1_bitmap.GetAddressOf());
-    d2d_dc->SetTarget(d2d1_bitmap.Get());
+    m_d2d_dc->CreateBitmapFromDxgiSurface(m_dxgi_surface.Get(), props, m_d2d_bitmap.GetAddressOf());
+    m_d2d_dc->SetTarget(m_d2d_bitmap.Get());
 
     // 5. Re-acquire the front buffer and surface references from the resized swapchain
-    dxgi_swapchain->GetBuffer(1, IID_PPV_ARGS(d3d11_front_buffer.GetAddressOf()));
-    dxgi_surface->QueryInterface(d3d11_surface.GetAddressOf());
+    m_dxgi_swapchain->GetBuffer(1, IID_PPV_ARGS(m_d3d_front_buffer.GetAddressOf()));
+    m_dxgi_surface->QueryInterface(m_d3d_surface.GetAddressOf());
 }
 
 void DCompPresenter::present()
 {
     // 1. Copy Direct2D graphics to the GDI-compatible texture
     Microsoft::WRL::ComPtr<ID3D11Resource> d2d_render_target;
-    dxgi_surface->QueryInterface(d2d_render_target.GetAddressOf());
-    d3d_dc->CopyResource(d3d11_gdi_tex.Get(), d2d_render_target.Get());
+    m_dxgi_surface->QueryInterface(d2d_render_target.GetAddressOf());
+    m_d3d_dc->CopyResource(m_d3d_gdi_tex.Get(), d2d_render_target.Get());
 
     // 2. Copy the GDI-compatible texture to the swapchain's back buffer
     Microsoft::WRL::ComPtr<ID3D11Resource> back_buffer;
-    dxgi_swapchain->GetBuffer(0, IID_PPV_ARGS(back_buffer.GetAddressOf()));
-    d3d_dc->CopyResource(back_buffer.Get(), d3d11_gdi_tex.Get());
+    m_dxgi_swapchain->GetBuffer(0, IID_PPV_ARGS(back_buffer.GetAddressOf()));
+    m_d3d_dc->CopyResource(back_buffer.Get(), m_d3d_gdi_tex.Get());
 
-    dxgi_swapchain->Present(0, 0);
-    comp_device->Commit();
+    m_dxgi_swapchain->Present(0, 0);
+    m_comp_device->Commit();
 }
 
 void DCompPresenter::blit(HDC hdc, RECT rect)
 {
     // 1. Get our GDI-compatible D3D texture's DC
     Microsoft::WRL::ComPtr<IDXGISurface1> dxgi_surface;
-    d3d11_gdi_tex->QueryInterface(dxgi_surface.GetAddressOf());
+    m_d3d_gdi_tex->QueryInterface(dxgi_surface.GetAddressOf());
 
     HDC dc;
     dxgi_surface->GetDC(false, &dc);
--- src/Views.Win32/lua/presenters/DCompPresenter.h
+++ src/Views.Win32/lua/presenters/DCompPresenter.h
@@ -22,24 +22,24 @@ class DCompPresenter : public Presenter
   private:
     HWND m_hwnd{};
     D2D1_SIZE_U m_size{};
-    Microsoft::WRL::ComPtr<IDXGIFactory2> dxgi_factory;
-    Microsoft::WRL::ComPtr<IDXGIAdapter1> dxgi_adapter;
-    Microsoft::WRL::ComPtr<IDXGIDevice1> dxgi_device;
-    Microsoft::WRL::ComPtr<IDXGISwapChain1> dxgi_swapchain;
-    Microsoft::WRL::ComPtr<IDXGISurface1> dxgi_surface;
+    Microsoft::WRL::ComPtr<IDXGIFactory2> m_dxgi_factory;
+    Microsoft::WRL::ComPtr<IDXGIAdapter1> m_dxgi_adapter;
+    Microsoft::WRL::ComPtr<IDXGIDevice1> m_dxgi_device;
+    Microsoft::WRL::ComPtr<IDXGISwapChain1> m_dxgi_swapchain;
+    Microsoft::WRL::ComPtr<IDXGISurface1> m_dxgi_surface;
 
-    Microsoft::WRL::ComPtr<ID3D11Device> d3d11_device;
-    Microsoft::WRL::ComPtr<ID3D11DeviceContext> d3d_dc;
-    Microsoft::WRL::ComPtr<ID3D11Resource> d3d11_surface;
-    Microsoft::WRL::ComPtr<ID3D11Resource> d3d11_front_buffer;
-    Microsoft::WRL::ComPtr<ID3D11Texture2D> d3d11_gdi_tex;
+    Microsoft::WRL::ComPtr<ID3D11Device> m_d3d_device;
+    Microsoft::WRL::ComPtr<ID3D11DeviceContext> m_d3d_dc;
+    Microsoft::WRL::ComPtr<ID3D11Resource> m_d3d_surface;
+    Microsoft::WRL::ComPtr<ID3D11Resource> m_d3d_front_buffer;
+    Microsoft::WRL::ComPtr<ID3D11Texture2D> m_d3d_gdi_tex;
 
-    Microsoft::WRL::ComPtr<ID2D1Bitmap1> d2d1_bitmap;
-    Microsoft::WRL::ComPtr<ID2D1Factory3> d2d_factory;
-    Microsoft::WRL::ComPtr<ID2D1Device2> d2d_device;
-    Microsoft::WRL::ComPtr<ID2D1DeviceContext2> d2d_dc;
+    Microsoft::WRL::ComPtr<ID2D1Bitmap1> m_d2d_bitmap;
+    Microsoft::WRL::ComPtr<ID2D1Factory3> m_d2d_factory;
+    Microsoft::WRL::ComPtr<ID2D1Device2> m_d2d_device;
+    Microsoft::WRL::ComPtr<ID2D1DeviceContext2> m_d2d_dc;
 
-    Microsoft::WRL::ComPtr<IDCompositionVisual> comp_visual;
-    Microsoft::WRL::ComPtr<IDCompositionDevice> comp_device;
-    Microsoft::WRL::ComPtr<IDCompositionTarget> comp_target;
+    Microsoft::WRL::ComPtr<IDCompositionVisual> m_comp_visual;
+    Microsoft::WRL::ComPtr<IDCompositionDevice> m_comp_device;
+    Microsoft::WRL::ComPtr<IDCompositionTarget> m_comp_target;
 };
