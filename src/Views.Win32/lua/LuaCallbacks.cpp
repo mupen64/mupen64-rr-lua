087c18e97610f49231447f816bb5a88c9c4c7d38
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2026-04-14 21:48:27 +0200

    no mutex

--- src/Views.Win32/lua/LuaCallbacks.cpp
+++ src/Views.Win32/lua/LuaCallbacks.cpp
@@ -8,18 +8,12 @@
 #include <lua/LuaCallbacks.h>
 #include <lua/LuaManager.h>
 
-// OPTIMIZATION: If no lua scripts are running, skip the deeper lua path
-// This is an unsynchronized access to the map from the emu thread!
-#define RET_IF_EMPTY                                                                                                   \
+#define RET_IF_NOT_REGISTERED(key)                                                                                     \
+    do                                                                                                                 \
     {                                                                                                                  \
         if (g_lua_environments.empty()) return;                                                                        \
-    }
-
-#define RET_IF_NOT_REGISTERED(key)                                                                                     \
-    {                                                                                                                  \
-        std::lock_guard<std::mutex> lock(m_ctx.mtx);                                                                   \
         if (m_ctx.callback_count_map[key] == 0) return;                                                                \
-    }
+    } while (false)
 
 struct t_atwindowmessage_context
 {
@@ -31,8 +25,7 @@ struct t_atwindowmessage_context
 
 struct LuaCallbacksContext
 {
-    std::mutex mtx;
-    std::unordered_map<LuaCallbacks::callback_key, size_t> callback_count_map;
+    std::unordered_map<LuaCallbacks::callback_key, std::atomic<size_t>> callback_count_map;
 
     LuaCallbacksContext()
     {
@@ -131,8 +124,7 @@ core_buttons LuaCallbacks::get_last_controller_data(int index)
 
 void LuaCallbacks::call_window_message(void *wnd, unsigned int msg, unsigned int w, long l)
 {
-    RET_IF_EMPTY;
-    RET_IF_NOT_REGISTERED(REG_WINDOWMESSAGE)
+    RET_IF_NOT_REGISTERED(REG_WINDOWMESSAGE);
 
     atwindowmessage_ctx = {.wnd = (HWND)wnd, .msg = msg, .w_param = w, .l_param = l};
 
@@ -141,19 +133,17 @@ void LuaCallbacks::call_window_message(void *wnd, unsigned int msg, unsigned int
 
 void LuaCallbacks::call_vi()
 {
-    RET_IF_EMPTY;
-    RET_IF_NOT_REGISTERED(REG_ATVI)
+    RET_IF_NOT_REGISTERED(REG_ATVI);
     g_main_ctx.dispatcher->invoke([] { invoke_callbacks_with_key_on_all_instances(REG_ATVI); });
 }
 
 void LuaCallbacks::call_input(core_buttons *input, int index)
 {
+    RET_IF_NOT_REGISTERED(REG_ATINPUT);
+
     // NOTE: Special callback, we store the input data for all scripts to access via joypad.get(n)
     // If they request a change via joypad.set(n, input), we change the input
     g_last_controller_data[index] = *input;
-
-    RET_IF_EMPTY;
-    RET_IF_NOT_REGISTERED(REG_ATINPUT)
 
     g_main_ctx.dispatcher->invoke([=] {
         current_input_n = index;
@@ -171,64 +161,55 @@ void LuaCallbacks::call_input(core_buttons *input, int index)
 
 void LuaCallbacks::call_interval()
 {
-    RET_IF_EMPTY;
-    RET_IF_NOT_REGISTERED(REG_ATINTERVAL)
+    RET_IF_NOT_REGISTERED(REG_ATINTERVAL);
     g_main_ctx.dispatcher->invoke([] { invoke_callbacks_with_key_on_all_instances(REG_ATINTERVAL); });
 }
 
 void LuaCallbacks::call_play_movie()
 {
-    RET_IF_EMPTY;
-    RET_IF_NOT_REGISTERED(REG_ATPLAYMOVIE)
+    RET_IF_NOT_REGISTERED(REG_ATPLAYMOVIE);
     g_main_ctx.dispatcher->invoke([] { invoke_callbacks_with_key_on_all_instances(REG_ATPLAYMOVIE); });
 }
 
 void LuaCallbacks::call_stop_movie()
 {
-    RET_IF_EMPTY;
-    RET_IF_NOT_REGISTERED(REG_ATSTOPMOVIE)
+    RET_IF_NOT_REGISTERED(REG_ATSTOPMOVIE);
     g_main_ctx.dispatcher->invoke([] { invoke_callbacks_with_key_on_all_instances(REG_ATSTOPMOVIE); });
 }
 
 void LuaCallbacks::call_load_state()
 {
-    RET_IF_EMPTY;
-    RET_IF_NOT_REGISTERED(REG_ATLOADSTATE)
+    RET_IF_NOT_REGISTERED(REG_ATLOADSTATE);
     g_main_ctx.dispatcher->invoke([] { invoke_callbacks_with_key_on_all_instances(REG_ATLOADSTATE); });
 }
 
 void LuaCallbacks::call_save_state()
 {
-    RET_IF_EMPTY;
-    RET_IF_NOT_REGISTERED(REG_ATSAVESTATE)
+    RET_IF_NOT_REGISTERED(REG_ATSAVESTATE);
     g_main_ctx.dispatcher->invoke([] { invoke_callbacks_with_key_on_all_instances(REG_ATSAVESTATE); });
 }
 
 void LuaCallbacks::call_reset()
 {
-    RET_IF_EMPTY;
-    RET_IF_NOT_REGISTERED(REG_ATRESET)
+    RET_IF_NOT_REGISTERED(REG_ATRESET);
     g_main_ctx.dispatcher->invoke([] { invoke_callbacks_with_key_on_all_instances(REG_ATRESET); });
 }
 
 void LuaCallbacks::call_seek_completed()
 {
-    RET_IF_EMPTY;
-    RET_IF_NOT_REGISTERED(REG_ATSEEKCOMPLETED)
+    RET_IF_NOT_REGISTERED(REG_ATSEEKCOMPLETED);
     g_main_ctx.dispatcher->invoke([] { invoke_callbacks_with_key_on_all_instances(REG_ATSEEKCOMPLETED); });
 }
 
 void LuaCallbacks::call_warp_modify_status_changed(const int32_t status)
 {
-    RET_IF_EMPTY;
-    RET_IF_NOT_REGISTERED(REG_ATWARPMODIFYSTATUSCHANGED)
+    RET_IF_NOT_REGISTERED(REG_ATWARPMODIFYSTATUSCHANGED);
     g_main_ctx.dispatcher->invoke([=] { invoke_callbacks_with_key_on_all_instances(REG_ATWARPMODIFYSTATUSCHANGED); });
 }
 
 void LuaCallbacks::call_atkey(const t_lua_key_event_args &args)
 {
-    RET_IF_EMPTY;
-    RET_IF_NOT_REGISTERED(REG_ATKEY)
+    RET_IF_NOT_REGISTERED(REG_ATKEY);
     atkey_ctx = args;
     g_main_ctx.dispatcher->invoke([=] { invoke_callbacks_with_key_on_all_instances(REG_ATKEY); });
 }
@@ -351,28 +332,18 @@ void LuaCallbacks::register_or_unregister_function(lua_State *l, const callback_
     {
         lua_pop(l, 1);
         unregister_function(l, key);
-
-        {
-            std::lock_guard<std::mutex> lock(m_ctx.mtx);
-            m_ctx.callback_count_map[key]--;
-        }
+        m_ctx.callback_count_map[key]--;
     }
     else
     {
         if (lua_gettop(l) == 2) lua_pop(l, 1);
         register_function(l, key);
-
-        {
-            std::lock_guard<std::mutex> lock(m_ctx.mtx);
-            m_ctx.callback_count_map[key]++;
-        }
+        m_ctx.callback_count_map[key]++;
     }
 }
 
 void LuaCallbacks::unregister_all(lua_State *l)
 {
-    std::lock_guard<std::mutex> lock(m_ctx.mtx);
-
     for (auto &[key, count] : m_ctx.callback_count_map)
     {
         lua_rawgeti(l, LUA_REGISTRYINDEX, key);
