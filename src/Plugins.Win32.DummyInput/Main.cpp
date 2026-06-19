227f57804ce84596d9d12b72b2d012c10fcf238f
abart27 <48759429+abart27@users.noreply.github.com>
2026-06-19 23:25:11 +0200

    feat(DummyInput)!: enable controller 1

--- src/Plugins.Win32.DummyInput/Main.cpp
+++ src/Plugins.Win32.DummyInput/Main.cpp
@@ -10,6 +10,21 @@
 #include <core_api.h>
 #include <Views.Win32/ViewPlugin.h>
 
-#define PLUGIN_NAME VERSION_NAME_HELPER_GEN_NAME(L"No Input", L"1.0.0")
+#define PLUGIN_NAME VERSION_NAME_HELPER_GEN_NAME(L"No Input", L"1.0.1")
 
 DUMMY_PLUGIN_STUB_IMPL(plugin_input)
+
+EXPORT void CALL InitiateControllers(core_input_info ControlInfo)
+{
+    auto *controllers = ControlInfo.controllers;
+
+    for (int i = 0; i < 4; ++i)
+    {
+        controllers[i].Present = 0;
+        controllers[i].RawData = 0;
+        controllers[i].Plugin = ce_none;
+    }
+
+    controllers[0].Present = 1;
+    controllers[0].Plugin = ce_mempak;
+}
