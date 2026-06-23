4376ea39f96d69e2df1e02d53ed3d46eb5173c6d
abart27 <48759429+abart27@users.noreply.github.com>
2026-06-23 22:06:21 +0200

    feat(TASRSP): remove config dialog (#759)
    microcode caching is not useful for perf

--- src/Plugins.Win32.TASRSP/Config.hpp
+++ src/Plugins.Win32.TASRSP/Config.hpp
@@ -11,18 +11,12 @@
 struct t_config
 {
     int32_t version = 2;
-    /**
-     * \brief Verify the cached ucode function on every audio ucode task. Enable this if you are debugging dynamic ucode
-     * changes.
-     */
-    int32_t ucode_cache_verify = false;
 
     friend void to_json(nlohmann::json &j, const t_config &self)
     {
 #define TASRSP_FIELD(field) {#field, self.field}
         j = nlohmann::json::object({
             TASRSP_FIELD(version),
-            TASRSP_FIELD(ucode_cache_verify),
         });
 #undef TASRSP_FIELD
     }
@@ -32,7 +26,6 @@ struct t_config
         if (!j.is_object()) throw std::domain_error("t_config expected JSON object");
 #define TASRSP_FIELD(field) nlohmann::from_json(j.at(#field), self.field)
         TASRSP_FIELD(version);
-        TASRSP_FIELD(ucode_cache_verify);
 #undef TASRSP_FIELD
     }
 };
@@ -48,5 +41,3 @@ void config_save();
  * \brief Loads the config
  */
 void config_load();
-
-void config_show_dialog(HWND hwnd);
--- src/Plugins.Win32.TASRSP/Resource.h
+++ src/Plugins.Win32.TASRSP/Resource.h
@@ -2,6 +2,4 @@
 // Von Microsoft Visual C++ generierte Includedatei.
 // Verwendet durch res.rc
 //
-#define IDD_RSPCONFIG 100
 #define IDC_STATIC 101
-#define IDC_UCODE_CACHE_VERIFY 108
