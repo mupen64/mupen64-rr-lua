3c9f4ef50c2b41aa253dfe882ea9ea5b88b17f6e
abart27 <48759429+abart27@users.noreply.github.com>
2026-06-24 13:10:07 +0200

    clean up memory module

--- src/Views.Win32/lua/modules/Memory.hpp
+++ src/Views.Win32/lua/modules/Memory.hpp
@@ -40,71 +40,81 @@ static void LuaPushQword(lua_State *L, ULONGLONG x)
 
 static int read_byte(lua_State *L)
 {
-    UCHAR value = core_rdram_load<UCHAR>((uint8_t *)g_main_ctx.core_ctx->rdram, luaL_checkinteger(L, 1));
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const auto value = core_rdram_load<UCHAR>((uint8_t *)g_main_ctx.core_ctx->rdram, addr);
     lua_pushinteger(L, value);
     return 1;
 }
 
 static int read_byte_signed(lua_State *L)
 {
-    CHAR value = core_rdram_load<CHAR>((uint8_t *)g_main_ctx.core_ctx->rdram, luaL_checkinteger(L, 1));
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const auto value = core_rdram_load<CHAR>((uint8_t *)g_main_ctx.core_ctx->rdram, addr);
     lua_pushinteger(L, value);
     return 1;
 }
 
 static int read_word(lua_State *L)
 {
-    USHORT value = core_rdram_load<USHORT>((uint8_t *)g_main_ctx.core_ctx->rdram, luaL_checkinteger(L, 1));
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const auto value = core_rdram_load<USHORT>((uint8_t *)g_main_ctx.core_ctx->rdram, addr);
     lua_pushinteger(L, value);
     return 1;
 }
 
 static int read_word_signed(lua_State *L)
 {
-    SHORT value = core_rdram_load<SHORT>((uint8_t *)g_main_ctx.core_ctx->rdram, luaL_checkinteger(L, 1));
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const auto value = core_rdram_load<SHORT>((uint8_t *)g_main_ctx.core_ctx->rdram, addr);
     lua_pushinteger(L, value);
     return 1;
 }
 
 static int read_dword(lua_State *L)
 {
-    ULONG value = core_rdram_load<ULONG>((uint8_t *)g_main_ctx.core_ctx->rdram, luaL_checkinteger(L, 1));
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const auto value = core_rdram_load<ULONG>((uint8_t *)g_main_ctx.core_ctx->rdram, addr);
     lua_pushinteger(L, value);
     return 1;
 }
 
 static int read_dword_signed(lua_State *L)
 {
-    LONG value = core_rdram_load<LONG>((uint8_t *)g_main_ctx.core_ctx->rdram, luaL_checkinteger(L, 1));
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const auto value = core_rdram_load<LONG>((uint8_t *)g_main_ctx.core_ctx->rdram, addr);
     lua_pushinteger(L, value);
     return 1;
 }
 
 static int read_qword(lua_State *L)
 {
-    ULONGLONG value = core_rdram_load<ULONGLONG>((uint8_t *)g_main_ctx.core_ctx->rdram, luaL_checkinteger(L, 1));
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const auto value = core_rdram_load<ULONGLONG>((uint8_t *)g_main_ctx.core_ctx->rdram, addr);
     LuaPushQword(L, value);
     return 1;
 }
 
 static int read_qword_signed(lua_State *L)
 {
-    LONGLONG value = core_rdram_load<LONGLONG>((uint8_t *)g_main_ctx.core_ctx->rdram, luaL_checkinteger(L, 1));
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const auto value = core_rdram_load<LONGLONG>((uint8_t *)g_main_ctx.core_ctx->rdram, addr);
     LuaPushQword(L, value);
     return 1;
 }
 
 static int read_float(lua_State *L)
 {
-    ULONG value = core_rdram_load<ULONG>((uint8_t *)g_main_ctx.core_ctx->rdram, luaL_checkinteger(L, 1));
-    lua_pushnumber(L, *(FLOAT *)&value);
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const auto value = std::bit_cast<FLOAT>(core_rdram_load<ULONG>((uint8_t *)g_main_ctx.core_ctx->rdram, addr));
+    lua_pushnumber(L, value);
     return 1;
 }
 
 static int read_double(lua_State *L)
 {
-    ULONGLONG value = core_rdram_load<ULONGLONG>((uint8_t *)g_main_ctx.core_ctx->rdram, luaL_checkinteger(L, 1));
-    lua_pushnumber(L, *(DOUBLE *)value);
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const auto value = std::bit_cast<DOUBLE>(core_rdram_load<ULONGLONG>((uint8_t *)g_main_ctx.core_ctx->rdram, addr));
+    lua_pushnumber(L, value);
     return 1;
 }
 
@@ -112,49 +122,58 @@ static int read_double(lua_State *L)
 
 static int write_byte(lua_State *L)
 {
-    core_rdram_store<UCHAR>((uint8_t *)g_main_ctx.core_ctx->rdram, luaL_checkinteger(L, 1), luaL_checkinteger(L, 2));
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const auto value = luaL_checkinteger(L, 2);
+    core_rdram_store<UCHAR>((uint8_t *)g_main_ctx.core_ctx->rdram, addr, value);
     return 0;
 }
 
 static int write_word(lua_State *L)
 {
-    core_rdram_store<USHORT>((uint8_t *)g_main_ctx.core_ctx->rdram, luaL_checkinteger(L, 1), luaL_checkinteger(L, 2));
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const auto value = luaL_checkinteger(L, 2);
+    core_rdram_store<USHORT>((uint8_t *)g_main_ctx.core_ctx->rdram, addr, value);
     return 0;
 }
 
 static int write_dword(lua_State *L)
 {
-    core_rdram_store<ULONG>((uint8_t *)g_main_ctx.core_ctx->rdram, luaL_checkinteger(L, 1), luaL_checkinteger(L, 2));
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const auto value = luaL_checkinteger(L, 2);
+    core_rdram_store<ULONG>((uint8_t *)g_main_ctx.core_ctx->rdram, addr, value);
     return 0;
 }
 
 static int write_qword(lua_State *L)
 {
-    core_rdram_store<ULONGLONG>((uint8_t *)g_main_ctx.core_ctx->rdram, luaL_checkinteger(L, 1), LuaCheckQWord(L, 2));
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const auto value = LuaCheckQWord(L, 2);
+    core_rdram_store<ULONGLONG>((uint8_t *)g_main_ctx.core_ctx->rdram, addr, value);
     return 0;
 }
 
 static int write_float(lua_State *L)
 {
-    FLOAT f = luaL_checknumber(L, -1);
-    core_rdram_store<ULONG>((uint8_t *)g_main_ctx.core_ctx->rdram, luaL_checkinteger(L, 1), *(ULONG *)&f);
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const FLOAT value = luaL_checknumber(L, 2);
+    core_rdram_store<ULONG>((uint8_t *)g_main_ctx.core_ctx->rdram, addr, std::bit_cast<ULONG>(value));
     return 0;
 }
 
 static int write_double(lua_State *L)
 {
-    DOUBLE f = luaL_checknumber(L, -1);
-    core_rdram_store<ULONGLONG>((uint8_t *)g_main_ctx.core_ctx->rdram, luaL_checkinteger(L, 1), *(ULONGLONG *)&f);
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const DOUBLE value = luaL_checknumber(L, 2);
+    core_rdram_store<ULONGLONG>((uint8_t *)g_main_ctx.core_ctx->rdram, addr, std::bit_cast<ULONGLONG>(value));
     return 0;
 }
 
 static int read_size(lua_State *L)
 {
-    ULONG addr = luaL_checkinteger(L, 1);
-    int size = luaL_checkinteger(L, 2);
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const int32_t size = luaL_checkinteger(L, 2);
     switch (size)
     {
-    // unsigned
     case 1:
         lua_pushinteger(L, core_rdram_load<UCHAR>((uint8_t *)g_main_ctx.core_ctx->rdram, addr));
         break;
@@ -167,7 +186,6 @@ static int read_size(lua_State *L)
     case 8:
         LuaPushQword(L, core_rdram_load<ULONGLONG>((uint8_t *)g_main_ctx.core_ctx->rdram, addr));
         break;
-    // signed
     case -1:
         lua_pushinteger(L, core_rdram_load<CHAR>((uint8_t *)g_main_ctx.core_ctx->rdram, addr));
         break;
@@ -188,8 +206,8 @@ static int read_size(lua_State *L)
 
 static int write_size(lua_State *L)
 {
-    ULONG addr = luaL_checkinteger(L, 1);
-    int size = luaL_checkinteger(L, 2);
+    const uint32_t addr = luaL_checkinteger(L, 1);
+    const int32_t size = luaL_checkinteger(L, 2);
     switch (size)
     {
     case 1:
@@ -224,57 +242,37 @@ static int write_size(lua_State *L)
 
 static int int_to_float(lua_State *L)
 {
-    ULONG n = luaL_checknumber(L, 1);
-    lua_pushnumber(L, *(FLOAT *)&n);
+    const ULONG n = luaL_checknumber(L, 1);
+    lua_pushnumber(L, std::bit_cast<FLOAT>(n));
     return 1;
 }
 
 static int int_to_double(lua_State *L)
 {
-    ULONGLONG n = LuaCheckQWord(L, 1);
-    lua_pushnumber(L, *(DOUBLE *)&n);
+    const ULONGLONG n = LuaCheckQWord(L, 1);
+    lua_pushnumber(L, std::bit_cast<DOUBLE>(n));
     return 1;
 }
 
 static int float_to_int(lua_State *L)
 {
-    FLOAT n = luaL_checknumber(L, 1);
-    lua_pushinteger(L, *(ULONG *)&n);
+    const FLOAT n = luaL_checknumber(L, 1);
+    lua_pushinteger(L, std::bit_cast<ULONG>(n));
     return 1;
 }
 
 static int double_to_int(lua_State *L)
 {
-    DOUBLE n = luaL_checknumber(L, 1);
-    LuaPushQword(L, *(ULONGLONG *)&n);
+    const DOUBLE n = luaL_checknumber(L, 1);
+    LuaPushQword(L, std::bit_cast<ULONGLONG>(n));
     return 1;
 }
 
 static int qword_to_number(lua_State *L)
 {
-    ULONGLONG n = LuaCheckQWord(L, 1);
+    const ULONGLONG n = LuaCheckQWord(L, 1);
     lua_pushnumber(L, n);
     return 1;
-}
-
-template <typename T> static void PushT(lua_State *L, T value)
-{
-    LuaPushIntU(L, value);
-}
-
-template <> static void PushT<ULONGLONG>(lua_State *L, ULONGLONG value)
-{
-    LuaPushQword(L, value);
-}
-
-template <typename T> static T CheckT(lua_State *L, int i)
-{
-    return LuaCheckIntegerU(L, i);
-}
-
-template <> static ULONGLONG CheckT<ULONGLONG>(lua_State *L, int i)
-{
-    return LuaCheckQWord(L, i);
 }
 
 static int recompile(lua_State *L)
