22bbb40bf253419285138d31b02489874b4369cc
abart27 <48759429+abart27@users.noreply.github.com>
2026-06-26 16:14:29 +0200

    feat: use only file config in plugins (#767)
    Might as well rip off the bandaid after
    53690dedff299fa065529e11bc527b40d2f9bcea
    
    changelog: skip

--- src/Plugins.Win32.TASRSP/Config.cpp
+++ src/Plugins.Win32.TASRSP/Config.cpp
@@ -7,8 +7,6 @@
 #include "Main.hpp"
 #include "Config.hpp"
 
-#define SUBKEY L"Software\\N64 Emulation\\DLL\\TAS RSP"
-#define CONFIG_VALUE L"Config"
 #define CONFIG_FILE_NAME "TASRSP.json"
 
 t_config config = {};
@@ -18,64 +16,6 @@ t_config prev_config = {};
 static std::filesystem::path get_config_path()
 {
     return g_config_path / CONFIG_FILE_NAME;
-}
-
-static void save_registry_config()
-{
-    g_ef->log_trace(L"Saving config...");
-
-    HKEY h_key{};
-
-    if (RegCreateKeyEx(HKEY_CURRENT_USER, SUBKEY, 0, NULL, 0, KEY_WRITE, NULL, &h_key, NULL) != ERROR_SUCCESS)
-    {
-        g_ef->log_error(L"RegCreateKeyEx failed");
-        return;
-    }
-
-    if (RegSetValueEx(h_key, CONFIG_VALUE, 0, REG_BINARY, reinterpret_cast<const BYTE *>(&config), sizeof(t_config)) !=
-        ERROR_SUCCESS)
-    {
-        g_ef->log_error(L"RegSetValueEx failed");
-        RegCloseKey(h_key);
-        return;
-    }
-
-    RegCloseKey(h_key);
-}
-
-static void load_registry_config()
-{
-    g_ef->log_trace(L"Loading config...");
-
-    HKEY h_key{};
-    DWORD size = sizeof(t_config);
-
-    if (RegOpenKeyEx(HKEY_CURRENT_USER, SUBKEY, 0, KEY_READ, &h_key) != ERROR_SUCCESS)
-    {
-        g_ef->log_error(L"RegCreateKeyEx failed");
-        return;
-    }
-
-    t_config loaded_config{};
-
-    if (RegQueryValueEx(h_key, CONFIG_VALUE, nullptr, nullptr, reinterpret_cast<BYTE *>(&loaded_config), &size) !=
-            ERROR_SUCCESS ||
-        size != sizeof(t_config))
-    {
-        g_ef->log_error(L"RegQueryValueEx failed");
-        RegCloseKey(h_key);
-        return;
-    }
-
-    RegCloseKey(h_key);
-
-    if (loaded_config.version < default_config.version)
-    {
-        g_ef->log_trace(L"Outdated config version, using default");
-        loaded_config = default_config;
-    }
-
-    config = loaded_config;
 }
 
 void config_save()
@@ -93,26 +33,23 @@ void config_load()
 
     auto json_path = get_config_path();
 
-    if (std::filesystem::exists(json_path))
-    {
-        std::ifstream ifs(json_path);
-        nlohmann::json j;
-
-        try
-        {
-            ifs >> j;
-            nlohmann::from_json(j, config);
-        }
-        catch (const std::exception &e)
-        {
-            g_ef->log_warn(L"Config load failed, using defaults...");
-            config = default_config;
-        }
-    }
-    else
-    {
-        g_ef->log_warn(L"No JSON config was present, attempting to load from registry");
-        load_registry_config();
+    if (!std::filesystem::exists(json_path)) {
+        config = default_config;
         config_save();
+        return;
+    }
+
+    std::ifstream ifs(json_path);
+    nlohmann::json j;
+
+    try
+    {
+        ifs >> j;
+        nlohmann::from_json(j, config);
+    }
+    catch (const std::exception &e)
+    {
+        g_ef->log_warn(L"Config load failed, using defaults...");
+        config = default_config;
     }
 }
