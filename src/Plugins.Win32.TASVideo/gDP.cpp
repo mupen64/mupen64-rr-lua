94b35a82c3948124ca5c62ba86ebce46f1513e7e
abart27 <48759429+abart27@users.noreply.github.com>
2026-06-30 14:27:55 +0200

    only set depthbuffer clear flag when appropriate

--- src/Plugins.Win32.TASVideo/gDP.cpp
+++ src/Plugins.Win32.TASVideo/gDP.cpp
@@ -595,9 +595,6 @@ void gDPSetScissor(u32 mode, f32 ulx, f32 uly, f32 lrx, f32 lry)
 
 void gDPFillRectangle(s32 ulx, s32 uly, s32 lrx, s32 lry)
 {
-    DepthBuffer *buffer = DepthBuffer_FindBuffer(gDP.colorImage.address);
-
-    if (buffer) buffer->cleared = TRUE;
 
     u32 fill_color = (u32)gDP.fillColor.r | ((u32)gDP.fillColor.g << 8) | ((u32)gDP.fillColor.b << 16) |
                      ((u32)gDP.fillColor.a << 24);
@@ -606,6 +603,8 @@ void gDPFillRectangle(s32 ulx, s32 uly, s32 lrx, s32 lry)
         (gDP.otherMode.cycleType == G_CYC_FILL && fill_color == 0x01010100))
     {
         OGL_ClearDepthBuffer();
+        DepthBuffer *buffer = DepthBuffer_FindBuffer(gDP.colorImage.address);
+        if (buffer) buffer->cleared = TRUE;
         return;
     }
 
