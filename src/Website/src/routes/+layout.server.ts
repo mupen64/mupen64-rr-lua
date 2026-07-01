a291563b3deaa5c39ac9fc2f871c73b29891a66e
abart27 <48759429+abart27@users.noreply.github.com>
2026-07-01 23:52:38 +0200

    feat(Website): various improvements (#791)

--- src/Website/src/routes/+layout.server.ts
+++ src/Website/src/routes/+layout.server.ts
@@ -1,7 +1,7 @@
 import { get_doc_names } from '$lib/helpers/DocFetcher';
 import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async () => {
-    return {
-        doc_names: await get_doc_names()
-    }
+	return {
+		doc_names: await get_doc_names()
+	};
 };
