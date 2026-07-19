a3ec8edae726841014d51edbab7aa347d372a052
abart27 <48759429+abart27@users.noreply.github.com>
2026-07-19 14:20:39 +0200

    remove old InitiateControllers

--- src/Views.Win32/include/Views.Win32/ZilmarExtSpecPlugin.h
+++ src/Views.Win32/include/Views.Win32/ZilmarExtSpecPlugin.h
@@ -364,11 +364,7 @@ extern "C"
     EXPORT void CALL ControllerCommand(int32_t Control, uint8_t *Command);
     EXPORT void CALL GetKeys(int32_t Control, CoreButtons *Keys);
     EXPORT void CALL SetKeys(int32_t controller, CoreButtons keys);
-#if defined(CORE_PLUGIN_INPUT_OLD_INITIATE_CONTROLLERS)
-    EXPORT void CALL InitiateControllers(void *hwnd, CoreController controls[4]);
-#else
     EXPORT void CALL InitiateControllers(ZilmarExtSpec::InputPluginInfo ControlInfo);
-#endif
     EXPORT void CALL ReadController(int Control, uint8_t *Command);
     EXPORT void CALL WM_KeyDown(uint32_t wParam, uint32_t lParam);
     EXPORT void CALL WM_KeyUp(uint32_t wParam, uint32_t lParam);
