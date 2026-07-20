d9124fc0659e7d208b8546e6078f5a041338319d
abart27 <48759429+abart27@users.noreply.github.com>
2026-07-20 19:16:42 +0200

    fix lifetime issues

--- src/Plugins.Win32.TASRSP/Main.cpp
+++ src/Plugins.Win32.TASRSP/Main.cpp
@@ -189,14 +189,14 @@ EXPORT void CALL M64RRGetMetadata(MupenRRSpecPlugin::PluginMetadata *metadata)
 {
     metadata->type = MupenRRSpecPlugin::PluginType::RSP;
 
-    const auto name = IOUtils::to_utf8_string(PLUGIN_NAME).c_str();
+    const auto name = IOUtils::to_utf8_string(PLUGIN_NAME);
     const auto description = "First-party TAS plugin for Mupen64."
                              "\n"
                              "TAS plugins are not to be distributed separately from Mupen64 and remain tied "
                              "to one version of the emulator."
                              "\n\n"
                              "https://mupen64.com";
-    const auto target_version = IOUtils::to_utf8_string(CURRENT_VERSION).c_str();
+    const auto target_version = IOUtils::to_utf8_string(CURRENT_VERSION);
 
     auto result = std::format_to_n(metadata->name, sizeof(metadata->name) - 1, "{}", name);
     metadata->name[result.size] = '\0';
