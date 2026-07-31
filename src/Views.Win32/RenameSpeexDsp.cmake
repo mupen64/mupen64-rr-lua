ee4a6b5800cb5dfcfa552229d92ab480d24030b6
abart27 <48759429+abart27@users.noreply.github.com>
2026-07-31 14:10:06 +0200

    fix build failure on native win

---
+++ src/Views.Win32/RenameSpeexDsp.cmake
@@ -0,0 +1,14 @@
+#[===[
+Copyright (c) 2026, Mupen64 maintainers, contributors, and original authors (Hacktarux, ShadowPrince, linker).
+
+SPDX-License-Identifier: GPL-2.0-or-later
+]===]
+
+if(EXISTS "${MUPEN64RR_OUT_DIR}/liblibspeexdsp.dll")
+    execute_process(
+        COMMAND "${CMAKE_COMMAND}" -E copy_if_different
+            "${MUPEN64RR_OUT_DIR}/liblibspeexdsp.dll"
+            "${MUPEN64RR_OUT_DIR}/libspeexdsp.dll"
+        RESULT_VARIABLE _speexdsp_copy_result
+    )
+endif()
