114fc82845b86ff4a71f1071e1fbbf94f8486927
jgcodes2020 <j349guo@uwaterloo.ca>
2026-07-31 10:26:29 -0400

    switch to features for c++ standards

--- src/Views.CLI/Main.cpp
+++ src/Views.CLI/Main.cpp
@@ -1,3 +1,10 @@
-int main(int argc, char* argv[]) {
+#include "Main.hpp"
 
+core_cfg g_config;
+core_params g_core_params {};
+core_ctx* g_core = nullptr;
+
+int main(int argc, char* argv[]) {
+  g_core_params.cfg = &g_config;
 }
+
---
+++ src/Views.CLI/Main.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <m64rr/API.hpp>
+
+extern core_cfg g_config;
+extern core_params g_core_params;
+extern core_ctx* g_core;
