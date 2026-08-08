1ece4f2bac23cd5800486daf928832ca2429930d
abart27 <48759429+abart27@users.noreply.github.com>
2026-08-08 19:04:40 +0200

    move SettingsListView to Common.Views.Win32
    unrelated but makes sense

--- src/Views.Win32/components/ConfigDialog.cpp
+++ src/Views.Win32/components/ConfigDialog.cpp
@@ -12,7 +12,7 @@
 #include <plugin/Plugin.hpp>
 #include <CaptureManager.hpp>
 #include <components/FilePicker.hpp>
-#include <components/SettingsListView.hpp>
+#include <SettingsListView.hpp>
 #include <components/TextEditDialog.hpp>
 #include <components/ConfigDialog.hpp>
 #include <lua/LuaManager.hpp>
