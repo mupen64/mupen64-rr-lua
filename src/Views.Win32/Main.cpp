d709973d6fe338b626d2d35a8141f890002b3e7f
abart27 <48759429+abart27@users.noreply.github.com>
2026-08-12 21:16:59 +0200

    fix: rare crashes in SDL_CreateWindow (#947)
    Seems like the strict handle check policy messes with SDL's internals
    indeed

--- src/Views.Win32/Main.cpp
+++ src/Views.Win32/Main.cpp
@@ -1033,12 +1033,6 @@ static bool is_dialog_message(MSG *msg)
  */
 static void enable_mitigations()
 {
-    PROCESS_MITIGATION_STRICT_HANDLE_CHECK_POLICY handles = {0};
-    handles.RaiseExceptionOnInvalidHandleReference = 1;
-    handles.HandleExceptionsPermanentlyEnabled = 1;
-    RT_ASSERT(SetProcessMitigationPolicy(ProcessStrictHandleCheckPolicy, &handles, sizeof(handles)),
-              L"Couldn't set process mitigation policy.");
-
     PROCESS_MITIGATION_EXTENSION_POINT_DISABLE_POLICY ext = {0};
     ext.DisableExtensionPoints = 1;
     RT_ASSERT(SetProcessMitigationPolicy(ProcessExtensionPointDisablePolicy, &ext, sizeof(ext)),
