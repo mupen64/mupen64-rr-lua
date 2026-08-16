4f6d314caa834c5395b26b8e71cdb32dd156fe76
abart27 <48759429+abart27@users.noreply.github.com>
2026-08-16 21:45:09 +0200

    fix thread affinity issues in m64rr plugins
    lifecycle events should be called from ui thread only

--- src/Views.Win32/plugin/M64RRPlugin.cpp
+++ src/Views.Win32/plugin/M64RRPlugin.cpp
@@ -36,6 +36,13 @@ static M64RRSpec::PtrDoRSPCycles s_mupenrr_do_rsp_cycles_fn = nullptr;
 #define LOOKUP_MUPENRR_FN(mupenrr_ptr, mupenrr_type, export_name)                                                      \
     mupenrr_ptr = (mupenrr_type)GetProcAddress(m_module, export_name);
 
+static void process_event_on_gui_thread(M64RRSpec::PtrProcessEvent event_fn, M64RRSpec::Event event)
+{
+    if (!event_fn) return;
+
+    g_main_ctx.dispatcher->invoke([event_fn, event] { event_fn(event); });
+}
+
 static size_t get_config_path(char *data, size_t size)
 {
     static const std::u8string config_path = std::filesystem::absolute(IOUtils::config_path()).u8string();
@@ -123,7 +130,8 @@ void M64RRPlugin::config(HWND hwnd)
         auto event_fn = (M64RRSpec::PtrProcessEvent)GetProcAddress(m_module, "M64RRProcessEvent");
         if (!event_fn) event_fn = [](auto) {};
 
-        event_fn(M64RRSpec::Event{.type = M64RRSpec::Event::Type::Shutdown});
+        process_event_on_gui_thread(event_fn,
+                                     M64RRSpec::Event{.type = M64RRSpec::Event::Type::Shutdown});
     }
 }
 
@@ -215,10 +223,10 @@ void M64RRPlugin::initiate(ZESpecFuncs &funcs)
         break;
     }
 
-    event_fn(M64RRSpec::Event{.initiate = {
-                                  .type = M64RRSpec::Event::Type::Initiate,
-                                  .init = init,
-                              }});
+    process_event_on_gui_thread(event_fn, M64RRSpec::Event{.initiate = {
+                                                            .type = M64RRSpec::Event::Type::Initiate,
+                                                            .init = init,
+                                                        }});
 
     switch (m_type)
     {
@@ -231,16 +239,16 @@ void M64RRPlugin::initiate(ZESpecFuncs &funcs)
         LOOKUP_MUPENRR_FN(s_mupenrr_read_video_fn, M64RRSpec::PtrReadVideo, "M64RRReadVideo");
 
         funcs.video_rom_open = []() {
-            if (s_mupenrr_video_event_fn)
-                s_mupenrr_video_event_fn(M64RRSpec::Event{.type = M64RRSpec::Event::Type::RomOpened});
+            process_event_on_gui_thread(s_mupenrr_video_event_fn,
+                                        M64RRSpec::Event{.type = M64RRSpec::Event::Type::RomOpened});
         };
         funcs.video_rom_closed = []() {
-            if (s_mupenrr_video_event_fn)
-                s_mupenrr_video_event_fn(M64RRSpec::Event{.type = M64RRSpec::Event::Type::RomClosed});
+            process_event_on_gui_thread(s_mupenrr_video_event_fn,
+                                        M64RRSpec::Event{.type = M64RRSpec::Event::Type::RomClosed});
         };
         funcs.video_close_dll = []() {
-            if (s_mupenrr_video_event_fn)
-                s_mupenrr_video_event_fn(M64RRSpec::Event{.type = M64RRSpec::Event::Type::Shutdown});
+            process_event_on_gui_thread(s_mupenrr_video_event_fn,
+                                        M64RRSpec::Event{.type = M64RRSpec::Event::Type::Shutdown});
         };
         funcs.video_process_dlist = []() {
             if (s_mupenrr_process_dlist_fn) s_mupenrr_process_dlist_fn();
@@ -274,20 +282,21 @@ void M64RRPlugin::initiate(ZESpecFuncs &funcs)
         g_view_logger->trace("Initiating audio plugin (MupenRR)...");
 
         LOOKUP_MUPENRR_FN(s_mupenrr_audio_event_fn, M64RRSpec::PtrProcessEvent, "M64RRProcessEvent");
+        LOOKUP_MUPENRR_FN(s_mupenrr_audio_get_windows_fn, M64RRSpec::PtrGetWindows, "M64RRGetWindows");
         LOOKUP_MUPENRR_FN(s_mupenrr_ai_dacrate_changed_fn, M64RRSpec::PtrAIDacrateChanged, "M64RRAIDacrateChanged");
         LOOKUP_MUPENRR_FN(s_mupenrr_ai_len_changed_fn, M64RRSpec::PtrAILenChanged, "M64RRAILenChanged");
 
         funcs.audio_rom_open = []() {
-            if (s_mupenrr_audio_event_fn)
-                s_mupenrr_audio_event_fn(M64RRSpec::Event{.type = M64RRSpec::Event::Type::RomOpened});
+            process_event_on_gui_thread(s_mupenrr_audio_event_fn,
+                                        M64RRSpec::Event{.type = M64RRSpec::Event::Type::RomOpened});
         };
         funcs.audio_rom_closed = []() {
-            if (s_mupenrr_audio_event_fn)
-                s_mupenrr_audio_event_fn(M64RRSpec::Event{.type = M64RRSpec::Event::Type::RomClosed});
+            process_event_on_gui_thread(s_mupenrr_audio_event_fn,
+                                        M64RRSpec::Event{.type = M64RRSpec::Event::Type::RomClosed});
         };
         funcs.audio_close_dll_audio = []() {
-            if (s_mupenrr_audio_event_fn)
-                s_mupenrr_audio_event_fn(M64RRSpec::Event{.type = M64RRSpec::Event::Type::Shutdown});
+            process_event_on_gui_thread(s_mupenrr_audio_event_fn,
+                                        M64RRSpec::Event{.type = M64RRSpec::Event::Type::Shutdown});
         };
         funcs.audio_ai_dacrate_changed = [](CoreSystemType system_type) {
             if (s_mupenrr_ai_dacrate_changed_fn) s_mupenrr_ai_dacrate_changed_fn(system_type);
@@ -311,16 +320,16 @@ void M64RRPlugin::initiate(ZESpecFuncs &funcs)
         LOOKUP_MUPENRR_FN(s_mupenrr_read_controller_fn, M64RRSpec::PtrReadController, "M64RRReadController");
 
         funcs.input_rom_open = []() {
-            if (s_mupenrr_input_event_fn)
-                s_mupenrr_input_event_fn(M64RRSpec::Event{.type = M64RRSpec::Event::Type::RomOpened});
+            process_event_on_gui_thread(s_mupenrr_input_event_fn,
+                                        M64RRSpec::Event{.type = M64RRSpec::Event::Type::RomOpened});
         };
         funcs.input_rom_closed = []() {
-            if (s_mupenrr_input_event_fn)
-                s_mupenrr_input_event_fn(M64RRSpec::Event{.type = M64RRSpec::Event::Type::RomClosed});
+            process_event_on_gui_thread(s_mupenrr_input_event_fn,
+                                        M64RRSpec::Event{.type = M64RRSpec::Event::Type::RomClosed});
         };
         funcs.input_close_dll = []() {
-            if (s_mupenrr_input_event_fn)
-                s_mupenrr_input_event_fn(M64RRSpec::Event{.type = M64RRSpec::Event::Type::Shutdown});
+            process_event_on_gui_thread(s_mupenrr_input_event_fn,
+                                        M64RRSpec::Event{.type = M64RRSpec::Event::Type::Shutdown});
         };
         funcs.input_controller_command = [](int32_t, uint8_t *) {};
         funcs.input_get_keys = [](int32_t controller, ZESpec::Buttons *keys) {
@@ -349,12 +358,12 @@ void M64RRPlugin::initiate(ZESpecFuncs &funcs)
 
         // FIXME: add rsp_rom_opened
         funcs.rsp_rom_closed = []() {
-            if (s_mupenrr_rsp_event_fn)
-                s_mupenrr_rsp_event_fn(M64RRSpec::Event{.type = M64RRSpec::Event::Type::RomClosed});
+            process_event_on_gui_thread(s_mupenrr_rsp_event_fn,
+                                        M64RRSpec::Event{.type = M64RRSpec::Event::Type::RomClosed});
         };
         funcs.rsp_close_dll = []() {
-            if (s_mupenrr_rsp_event_fn)
-                s_mupenrr_rsp_event_fn(M64RRSpec::Event{.type = M64RRSpec::Event::Type::Shutdown});
+            process_event_on_gui_thread(s_mupenrr_rsp_event_fn,
+                                        M64RRSpec::Event{.type = M64RRSpec::Event::Type::Shutdown});
         };
         funcs.rsp_do_rsp_cycles = [](uint32_t cycles) {
             if (s_mupenrr_do_rsp_cycles_fn)
