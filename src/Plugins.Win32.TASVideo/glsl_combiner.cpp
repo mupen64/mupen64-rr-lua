f3800f456a0e6b7ad03de2fcfdb8cb88d42bb812
jgcodes2020 <j349guo@uwaterloo.ca>
2026-08-19 00:46:40 -0400

    check for copyright headers in CI

--- src/Plugins.Win32.TASVideo/glsl_combiner.cpp
+++ src/Plugins.Win32.TASVideo/glsl_combiner.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2026, Mupen64 Organization (https://github.com/mupen64)
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
 #include "Common.hpp"
 #include "glsl_combiner.hpp"
 #include "Combiner.hpp"
