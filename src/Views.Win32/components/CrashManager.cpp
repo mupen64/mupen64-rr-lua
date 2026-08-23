1bfa70a4e4719477d729b3842041cb0830df87f7
abart27 <aurumaker@proton.me>
2026-08-23 10:08:44 +0200

    cpp exception unwinding crash manager

--- src/Views.Win32/components/CrashManager.cpp
+++ src/Views.Win32/components/CrashManager.cpp
@@ -8,12 +8,12 @@
 #include <Common.Views/Config.hpp>
 #include <components/CrashManager.hpp>
 
-typedef struct StacktraceInfo
-{
-    void *rtl_stacktrace[32]{};
-} t_stacktrace_info;
-
-static t_stacktrace_info stacktrace_info;
+#include <cstdlib>
+#include <exception>
+#include <typeinfo>
+#if !defined(_MSC_VER)
+#include <cxxabi.h>
+#endif
 
 #define E(x) {x, #x}
 const std::unordered_map<int, std::string> EXCEPTION_NAMES = {
@@ -43,30 +43,16 @@ const std::unordered_map<int, std::string> EXCEPTION_NAMES = {
 };
 #undef E
 
+typedef struct StacktraceInfo
+{
+    void *rtl_stacktrace[32]{};
+} t_stacktrace_info;
+
+static t_stacktrace_info stacktrace_info;
+
 static std::filesystem::path get_minidump_path()
 {
     return IOUtils::exe_path().parent_path() / "logs" / "mupen.dmp";
-}
-
-void create_minidump(EXCEPTION_POINTERS *e)
-{
-    MINIDUMP_EXCEPTION_INFORMATION info{};
-
-    const auto minidump_path = get_minidump_path();
-    const HANDLE h_dump_file = CreateFile(minidump_path.string().c_str(), GENERIC_WRITE,
-                                          FILE_SHARE_WRITE | FILE_SHARE_READ, 0, CREATE_ALWAYS, 0, 0);
-
-    info.ThreadId = GetCurrentThreadId();
-    info.ExceptionPointers = e;
-    info.ClientPointers = TRUE;
-
-    if (!MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), h_dump_file, MiniDumpWithDataSegs, &info, NULL,
-                           NULL))
-    {
-        g_view_logger->error("Couldn't create minidump (error code: {})", GetLastError());
-    }
-
-    CloseHandle(h_dump_file);
 }
 
 /**
@@ -113,6 +99,28 @@ static std::string get_exception_code_friendly_name(const _EXCEPTION_POINTERS *e
     return std::format("{} ({:#08x})", exception_name, e->ExceptionRecord->ExceptionCode);
 }
 
+static void create_minidump(EXCEPTION_POINTERS *e)
+{
+    if (!e) return;
+    MINIDUMP_EXCEPTION_INFORMATION info{};
+
+    const auto minidump_path = get_minidump_path();
+    const HANDLE h_dump_file = CreateFile(minidump_path.string().c_str(), GENERIC_WRITE,
+                                          FILE_SHARE_WRITE | FILE_SHARE_READ, 0, CREATE_ALWAYS, 0, 0);
+
+    info.ThreadId = GetCurrentThreadId();
+    info.ExceptionPointers = e;
+    info.ClientPointers = TRUE;
+
+    if (!MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), h_dump_file, MiniDumpWithDataSegs, &info, NULL,
+                           NULL))
+    {
+        g_view_logger->error("Couldn't create minidump (error code: {})", GetLastError());
+    }
+
+    CloseHandle(h_dump_file);
+}
+
 static __forceinline void fill_stacktrace_info()
 {
     stacktrace_info = {};
@@ -120,7 +128,7 @@ static __forceinline void fill_stacktrace_info()
     CaptureStackBackTrace(0, std::size(stacktrace_info.rtl_stacktrace), stacktrace_info.rtl_stacktrace, NULL);
 }
 
-static void log_crash(const std::string &additional_exception_info)
+static void log_crash(std::string_view additional_info)
 {
     SYSTEMTIME time;
     GetSystemTime(&time);
@@ -135,7 +143,7 @@ static void log_crash(const std::string &additional_exception_info)
     g_view_logger->critical("RSP: {}", g_config.selected_rsp_plugin);
     g_view_logger->critical("VCR Task: {}", static_cast<int>(g_main_ctx.core_ctx->vcr_get_task()));
     g_view_logger->critical("Core Executing: {}", g_main_ctx.core_ctx->vr_get_launched());
-    g_view_logger->critical(additional_exception_info);
+    g_view_logger->critical(additional_info);
 
     g_view_logger->critical("RTL Stacktrace:");
     for (auto i = 0; i < std::size(stacktrace_info.rtl_stacktrace); ++i)
@@ -151,63 +159,28 @@ static void log_crash(const std::string &additional_exception_info)
     g_view_logger->flush();
 }
 
-bool show_crash_dialog(bool continuable)
+void show_crash_dialog(std::string_view additional_info)
 {
     if (g_config.silent_mode)
     {
-        return true;
+        return;
     }
 
-    int result = 0;
-    if (continuable)
-    {
-        TaskDialog(g_main_ctx.hwnd, g_main_ctx.hinst, L"Error", L"An error has occured",
-                   L"Crash dumps have been automatically generated. You can choose to continue program execution.",
-                   TDCBF_RETRY_BUTTON | TDCBF_CLOSE_BUTTON, TD_ERROR_ICON, &result);
-    }
-    else
-    {
-        TaskDialog(g_main_ctx.hwnd, g_main_ctx.hinst, L"Error", L"An error has occured",
-                   L"Crash dumps have been automatically generated. The program will now exit.", TDCBF_CLOSE_BUTTON,
-                   TD_ERROR_ICON, &result);
-    }
+    const auto detail = IOUtils::to_wide_string(additional_info);
 
-    return result == IDCLOSE;
-}
+    TASKDIALOGCONFIG config{};
+    config.cbSize = sizeof(config);
+    config.hwndParent = g_main_ctx.hwnd;
+    config.hInstance = g_main_ctx.hinst;
+    config.pszWindowTitle = L"Error";
+    config.pszMainIcon = TD_ERROR_ICON;
+    config.pszMainInstruction = L"An error has occured";
+    config.dwCommonButtons = TDCBF_CLOSE_BUTTON;
+    config.pszContent = L"Crash dumps have been automatically generated.\nThe program will now exit.";
+    config.pszExpandedInformation = detail.c_str();
+    config.dwFlags = TDF_EXPANDED_BY_DEFAULT;
 
-LONG WINAPI exception_handler(_EXCEPTION_POINTERS *e)
-{
-    fill_stacktrace_info();
-
-    std::string exception_info;
-    exception_info += get_metadata_for_exception_address(e->ExceptionRecord->ExceptionAddress) + " " +
-                      get_exception_code_friendly_name(e) + " ";
-    exception_info += "(from SetUnhandledExceptionFilter) ";
-    log_crash(exception_info);
-
-    create_minidump(e);
-
-    const bool is_continuable = !(e->ExceptionRecord->ExceptionFlags & EXCEPTION_NONCONTINUABLE);
-
-    const bool close = show_crash_dialog(is_continuable);
-
-    return close ? EXCEPTION_EXECUTE_HANDLER : EXCEPTION_CONTINUE_EXECUTION;
-}
-
-void invalid_parameter_handler(const wchar_t *expression, const wchar_t *function, const wchar_t *file,
-                               unsigned int line, uintptr_t)
-{
-    fill_stacktrace_info();
-
-    std::string exception_info;
-    exception_info += std::format("File: {} ", file ? IOUtils::to_utf8_string(file) : "(unknown)");
-    exception_info += std::format("Function: {} ", function ? IOUtils::to_utf8_string(function) : "(unknown)");
-    exception_info += std::format("Expression: {} ", expression ? IOUtils::to_utf8_string(expression) : "(unknown)");
-    exception_info += std::format("Line: {} ", line);
-    exception_info += "(from _set_invalid_parameter_handler) ";
-
-    log_crash(exception_info);
-    show_crash_dialog(false);
+    TaskDialogIndirect(&config, nullptr, nullptr, nullptr);
 }
 
 // See https://randomascii.wordpress.com/2012/07/05/when-even-crashing-doesnt-work/
@@ -230,22 +203,113 @@ static void enable_crashing_on_crashes()
     }
 }
 
+static std::string demangle_type_name(const char *name)
+{
+#if defined(_MSC_VER)
+    return name;
+#else
+    int status = 0;
+    char *demangled = abi::__cxa_demangle(name, nullptr, nullptr, &status);
+    if (demangled)
+    {
+        const std::string result(demangled);
+        std::free(demangled);
+        return result;
+    }
+    return name;
+#endif
+}
+
+static LONG WINAPI exception_handler(_EXCEPTION_POINTERS *e)
+{
+    // C++ exception, let it fall through to terminate handler
+    if (e->ExceptionRecord->ExceptionCode == 0x20474343) return EXCEPTION_CONTINUE_EXECUTION;
+
+    fill_stacktrace_info();
+    create_minidump(e);
+
+    std::string info = std::format("from SetUnhandledExceptionFilter: {} {}", get_exception_code_friendly_name(e),
+                                   get_metadata_for_exception_address(e->ExceptionRecord->ExceptionAddress));
+    log_crash(info);
+    show_crash_dialog(info);
+    return EXCEPTION_EXECUTE_HANDLER;
+}
+
+static void invalid_parameter_handler(const wchar_t *expression, const wchar_t *function, const wchar_t *file,
+                                      unsigned int line, uintptr_t)
+{
+    fill_stacktrace_info();
+
+    std::string info;
+    info += std::format("File: {} ", file ? IOUtils::to_utf8_string(file) : "(unknown)");
+    info += std::format("Function: {} ", function ? IOUtils::to_utf8_string(function) : "(unknown)");
+    info += std::format("Expression: {} ", expression ? IOUtils::to_utf8_string(expression) : "(unknown)");
+    info += std::format("Line: {} ", line);
+    info += "(from _set_invalid_parameter_handler) ";
+
+    log_crash(info);
+    show_crash_dialog(info);
+}
+
+static void terminate_handler()
+{
+    fill_stacktrace_info();
+
+    std::string info;
+
+    const auto format_exception_info = [](const char *msg, const char *type) {
+        return std::format("{}\n\nFrom {}", msg ? msg : "(no exception message)",
+                           type ? type : "(unknown exception type)");
+    };
+
+    try
+    {
+        std::rethrow_exception(std::current_exception());
+    }
+    catch (const std::exception &e)
+    {
+        info = format_exception_info(e.what(), demangle_type_name(typeid(e).name()).c_str());
+    }
+    catch (const std::exception *e)
+    {
+        info = format_exception_info(e ? e->what() : "(null)", demangle_type_name(typeid(e).name()).c_str());
+    }
+    catch (const std::wstring &w)
+    {
+        info = format_exception_info(IOUtils::to_utf8_string(w).c_str(), demangle_type_name(typeid(w).name()).c_str());
+    }
+    catch (const std::string &s)
+    {
+        info = format_exception_info(s.c_str(), demangle_type_name(typeid(s).name()).c_str());
+    }
+    catch (const char *msg)
+    {
+        info = format_exception_info(msg, demangle_type_name(typeid(msg).name()).c_str());
+    }
+    catch (...)
+    {
+        std::string type_name = "(unknown)";
+#if !defined(_MSC_VER)
+        if (const std::type_info *ti = abi::__cxa_current_exception_type(); ti)
+            type_name = demangle_type_name(ti->name());
+#endif
+        info = format_exception_info(nullptr, type_name.c_str());
+    }
+
+    log_crash(info);
+    show_crash_dialog(info);
+
+    std::abort();
+}
+
 void CrashManager::init()
 {
     enable_crashing_on_crashes();
 
     SetUnhandledExceptionFilter(exception_handler);
     _set_invalid_parameter_handler(invalid_parameter_handler);
+    std::set_terminate(terminate_handler);
 
-    // Some test cases:
-    //
-    // RaiseException(EXCEPTION_ACCESS_VIOLATION, EXCEPTION_NONCONTINUABLE, NULL, NULL);
-    //
-    // RaiseException(EXCEPTION_ACCESS_VIOLATION, 0, NULL, NULL);
-    //
-    // printf(0);
-    //
-    // In WndProc:
-    // int* a = 0; *a = 1;
-    //
+    // std::thread([] { throw new std::logic_error("huh!!!"); }).detach();
+    // RaiseException(EXCEPTION_BREAKPOINT, 0, 0, nullptr);
 }
