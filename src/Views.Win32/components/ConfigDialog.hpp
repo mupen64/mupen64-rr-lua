6925b5b70ea51e5a74bac98fefad0b3ffa581ebd
abart27 <aurumaker@proton.me>
2026-08-26 22:46:19 +0200

    raw move option item/group to Common.Views

--- src/Views.Win32/components/ConfigDialog.hpp
+++ src/Views.Win32/components/ConfigDialog.hpp
@@ -7,132 +7,13 @@
 #pragma once
 
 #include <Common/Assert.hpp>
+#include <Common.Views/OptionItem.hpp>
 
 /**
  * \brief A module responsible for implementing configuration dialogs.
  */
 namespace ConfigDialog
 {
-/**
- * Represents a settings option.
- */
-struct t_options_item
-{
-    enum class Type
-    {
-        Bool,
-        Number,
-        Enum,
-        String,
-        Hotkey,
-        Folder,
-    };
-
-    typedef std::variant<int32_t, double, std::string, Hotkey> data_variant;
-
-    struct t_readonly_property
-    {
-        std::function<data_variant()> get{};
-
-        explicit t_readonly_property(const std::function<data_variant()> &get) { this->get = get; }
-    };
-
-    struct t_readwrite_property : t_readonly_property
-    {
-        std::function<void(const data_variant &)> set{};
-
-        t_readwrite_property(
-            const std::function<data_variant()> &get, const std::function<void(const data_variant &)> &set)
-            : t_readonly_property(get)
-        {
-            this->set = set;
-        }
-    };
-
-    /**
-     * The option's backing data type.
-     */
-    Type type;
-
-    /**
-     * The group this option belongs to.
-     */
-    size_t group_id;
-
-    /**
-     * The option's display name.
-     */
-    std::string name{};
-
-    /**
-     * The option's tooltip, or an empty string if no tooltip is set.
-     */
-    std::string tooltip{};
-
-    t_readwrite_property current_value;
-
-    t_readonly_property initial_value = t_readonly_property([] -> data_variant {
-        NEED(false, "Initial value not set for option");
-        return data_variant{};
-    });
-
-    t_readonly_property default_value;
-
-    std::vector<std::pair<std::string, int32_t>> possible_values;
-
-    /**
-     * Function which returns why an option is read-only, or std::nullopt if it is not read-only.
-     */
-    std::function<std::optional<std::string>()> get_readonly_reason = [] { return std::nullopt; };
-
-    /**
-     * Gets the name of the option item.
-     */
-    [[nodiscard]] std::string get_name() const;
-
-    /**
-     * Gets the value name for the current backing data, or a fallback name if no match is found.
-     */
-    [[nodiscard]] std::string get_value_name() const;
-
-    /**
-     * Resets the value of the option to the default value.
-     */
-    void reset_to_default() const;
-
-    /**
-     * \brief Gets neatly formatted information about the option.
-     */
-    std::string get_friendly_info() const;
-
-    /**
-     * \brief Prompts the user to edit the option value.
-     * \param hwnd The parent window handle for any dialogs spawned by this method.
-     * \return Whether the user confirmed the edit.
-     */
-    bool edit(HWND hwnd);
-};
-
-/**
- * Represents a group of options in the settings.
- */
-struct t_options_group
-{
-    /**
-     * The group's unique identifier.
-     */
-    size_t id;
-
-    /**
-     * The group's name.
-     */
-    std::string name;
-
-    /**
-     * \brief The options that belong to this group.
-     */
-    std::vector<t_options_item> items{};
-};
 
 /**
  * \brief Shows the application settings dialog.
