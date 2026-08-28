4614f890c455676bb061593da873ef5e4935a772
abart27 <aurumaker@proton.me>
2026-08-28 21:38:20 +0200

    build: rename CMake targets to valid C identifiers (#1005)
    Closes #1004
    
    ---------
    
    Co-authored-by: jgcodes2020 <j349guo@uwaterloo.ca>

--- src/Plugins.Common.cmake
+++ src/Plugins.Common.cmake
@@ -7,12 +7,12 @@ SPDX-License-Identifier: GPL-2.0-or-later
 #
 
 # Common Win32 plugin dependencies.
-add_library(Mupen64RR.Plugins.Common INTERFACE)
-target_link_libraries(Mupen64RR.Plugins.Common INTERFACE
-    Mupen64RR.Common
-    Mupen64RR.Core.Headers
+add_library(Mupen64RR_Plugins_Common INTERFACE)
+target_link_libraries(Mupen64RR_Plugins_Common INTERFACE
+    Mupen64RR_Common
+    Mupen64RR_Core_Headers
 )
-target_compile_definitions(Mupen64RR.Plugins.Common INTERFACE
+target_compile_definitions(Mupen64RR_Plugins_Common INTERFACE
     PLUGIN_WITH_CALLBACKS
     "VERSION_SUFFIX=\"${MUPEN64RR_VERSION_SUFFIX}\""
     "$<$<BOOL:${MUPEN64RR_NIGHTLY}>:NIGHTLY>"
