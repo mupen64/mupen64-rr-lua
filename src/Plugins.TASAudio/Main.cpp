130e7c07f85c7efebe6b35e89360e0b5865fe917
abart27 <aurumaker@proton.me>
2026-08-28 18:42:21 +0200

    refactor: remove `plugin_version` field from M64RR plugin spec
    built-in plugins are statically linked; this is not necessary anymore

--- src/Plugins.TASAudio/Main.cpp
+++ src/Plugins.TASAudio/Main.cpp
@@ -87,16 +87,12 @@ EXPORT void CALL M64RRGetMetadata(M64RRSpec::PluginMetadata *metadata)
     const auto description = "Built-in plugin for Mupen64."
                              "\n\n"
                              "https://mupen64.com";
-    const auto target_version = CURRENT_VERSION;
 
     auto result = std::format_to_n(metadata->name, sizeof(metadata->name) - 1, "{}", name);
     metadata->name[result.size] = '\0';
 
     result = std::format_to_n(metadata->description, sizeof(metadata->description) - 1, "{}", description);
     metadata->description[result.size] = '\0';
-
-    result = std::format_to_n(metadata->target_version, sizeof(metadata->target_version) - 1, "{}", target_version);
-    metadata->target_version[result.size] = '\0';
 }
 
 EXPORT void CALL M64RRProcessEvent(Event event)
