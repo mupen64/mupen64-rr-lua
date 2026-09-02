ce6264e1be72e72919d45b8afcd1cd5fec2a1f84
abart27 <aurumaker@proton.me>
2026-09-02 23:40:13 +0200

    feat(Seeker): show ETA
    Closes #287

--- src/Views.Win32/components/Seeker.cpp
+++ src/Views.Win32/components/Seeker.cpp
@@ -16,6 +16,7 @@ struct seeker_state
 {
     HWND hwnd{};
     UINT_PTR refresh_timer{};
+    std::uint64_t seek_start_tick{};
 };
 
 static seeker_state seeker{};
@@ -34,6 +35,27 @@ static INT_PTR CALLBACK dlgproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpara
 
         if (g_config.core.seek_savestate_interval == 0)
             SetDlgItemText(hwnd, IDC_SEEKER_SUBTEXT, "Seek savestates disabled. Seeking backwards will be slower.");
+        else
+        {
+            ShowWindow(GetDlgItem(hwnd, IDC_SEEKER_SUBTEXT), SW_HIDE);
+
+            RECT shrink = {0, 0, 0, 13};
+            MapDialogRect(hwnd, &shrink);
+
+            for (const int id : {IDC_SEEKER_STOP, IDC_SEEKER_START, IDCANCEL})
+            {
+                const HWND item = GetDlgItem(hwnd, id);
+                RECT rc;
+                GetWindowRect(item, &rc);
+                MapWindowPoints(nullptr, hwnd, reinterpret_cast<POINT *>(&rc), 2);
+                SetWindowPos(item, nullptr, rc.left, rc.top - shrink.bottom, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+            }
+
+            RECT window;
+            GetWindowRect(hwnd, &window);
+            SetWindowPos(hwnd, nullptr, 0, 0, window.right - window.left, window.bottom - window.top - shrink.bottom,
+                SWP_NOMOVE | SWP_NOZORDER);
+        }
 
         SetFocus(GetDlgItem(hwnd, IDC_SEEKER_FRAME));
         WinDarkMode::attach(hwnd);
@@ -50,6 +72,7 @@ static INT_PTR CALLBACK dlgproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpara
         break;
     case WM_SEEK_COMPLETED:
         SetDlgItemText(hwnd, IDC_SEEKER_STATUS, "Seek completed");
+        SetDlgItemText(hwnd, IDC_SEEKER_ETA, "");
         EnableWindow(GetDlgItem(hwnd, IDC_SEEKER_STOP), FALSE);
         KillTimer(hwnd, seeker.refresh_timer);
         break;
@@ -64,6 +87,13 @@ static INT_PTR CALLBACK dlgproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpara
             static_cast<float>(info.seek_start_sample), static_cast<float>(info.seek_target_sample), 0.0f, 1.0f);
         const auto str = std::format("Seeked {:.2f}%", effective_progress * 100.0);
         SetDlgItemText(hwnd, IDC_SEEKER_STATUS, str.c_str());
+
+        if (effective_progress > 0.0f)
+        {
+            const double elapsed = static_cast<double>(GetTickCount64() - seeker.seek_start_tick) / 1000.0;
+            const double eta = elapsed * (1.0 - effective_progress) / effective_progress;
+            SetDlgItemText(hwnd, IDC_SEEKER_ETA, format_eta(eta).c_str());
+        }
         break;
     }
     case WM_COMMAND:
@@ -90,6 +120,8 @@ static INT_PTR CALLBACK dlgproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpara
             if (is_seeking)
             {
                 SetFocus(GetDlgItem(hwnd, IDC_SEEKER_STOP));
+                seeker.seek_start_tick = GetTickCount64();
+                SetDlgItemText(hwnd, IDC_SEEKER_ETA, "");
                 seeker.refresh_timer = SetTimer(hwnd, 0, 1000 / 10, nullptr);
             }
 
